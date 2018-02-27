98f21abace9877fca0973a83f7431840cfb20464
Jon Smith <jon.smith@crick.ac.uk>
2018-02-27 17:58:38 +0000

    fix up indentation

--- wkcuber/metadata.py
+++ wkcuber/metadata.py
@@ -29,13 +29,15 @@ def write_webknossos_metadata(dataset_base_path,
                     'category': layer_type,
                     'elementClass': dtype,
                     'sections': [
-                        'name': '',
-                        'resolutions': resolutions,
-                        'boundingBox': {
-                            "topLeft": [0, 0, 0],
-                            "width": bbox[0],
-                            "height": bbox[1],
-                            "depth": bbox[2]
+                        {
+                            'name': '',
+                            'resolutions': resolutions,
+                            'boundingBox': {
+                                "topLeft": [0, 0, 0],
+                                "width": bbox[0],
+                                "height": bbox[1],
+                                "depth": bbox[2]
+                            }
                         }
                     ]
                 }
