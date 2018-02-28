dcb0af147f2d6a80fec88ce5bb8d109cf3122da9
Norman Rzepka <code@normanrz.com>
2018-02-28 13:33:33 +0100

    Squashed commit of the following:
    commit 01c1d392c93d611b0b1613bea54300992688799a
    Author: Norman Rzepka <code@normanrz.com>
    Date:   Wed Feb 28 13:31:24 2018 +0100
    
        fix test
    
    commit 0a149dc4a547457f35251df8aa5d6c8ae8b0e808
    Author: Norman Rzepka <code@normanrz.com>
    Date:   Wed Feb 28 13:15:50 2018 +0100
    
        adds fixture
    
    commit 22edc87dbc96a44047fb3623b3bb2c7a91c5e36c
    Author: Norman Rzepka <code@normanrz.com>
    Date:   Wed Feb 28 13:13:22 2018 +0100
    
        adds datasource-properties test
    
    commit 5a1ad6575458dcc7c3bd574b20c3037acfc86602
    Author: Norman Rzepka <code@normanrz.com>
    Date:   Wed Feb 28 13:08:57 2018 +0100
    
        fix typo + remove old metadata files
    
    commit 98f21abace9877fca0973a83f7431840cfb20464
    Author: Jon Smith <jon.smith@crick.ac.uk>
    Date:   Tue Feb 27 17:58:38 2018 +0000
    
        fix up indentation
    
    commit a3fff4300434051213226a51e3175832c414b0fd
    Author: JonathanCSmith <jon@jonathansmith.me>
    Date:   Tue Feb 27 15:57:05 2018 +0000
    
        Found where data type comes from!
    
    commit bf59a53d2b9fa7cd6e82fdf25f17142de4cfd81e
    Author: JonathanCSmith <jon@jonathansmith.me>
    Date:   Tue Feb 27 15:55:51 2018 +0000
    
        Update metadata format to work with updated webknossos data API
    
        added the new datasource-properties.json to be generated directly.

--- wkcuber/metadata.py
+++ wkcuber/metadata.py
@@ -14,32 +14,34 @@ def write_webknossos_metadata(dataset_base_path,
     if not path.exists(dataset_base_path):
         makedirs(dataset_base_path)
 
-    settings_json_path = path.join(dataset_base_path, 'settings.json')
-    with open(settings_json_path, 'wt') as settings_json:
+    # Generate a combined metadata file (for webknossos). Currently have no source of information for team
+    datasource_properties_path = path.join(dataset_base_path, 'datasource-properties.json')
+    with open(datasource_properties_path, 'wt') as datasource_properties_json:
         json.dump({
-            'name': name,
-            'priority': 0,
+            'id': {
+                'name': name,
+                'team': '<unknown>',
+            },
+            'dataLayers': [
+                {
+                    'dataFormat': 'knossos',
+                    'name': layer_name,
+                    'category': layer_type,
+                    'elementClass': dtype,
+                    'sections': [
+                        {
+                            'name': '',
+                            'resolutions': resolutions,
+                            'boundingBox': {
+                                "topLeft": [0, 0, 0],
+                                "width": bbox[0],
+                                "height": bbox[1],
+                                "depth": bbox[2]
+                            }
+                        }
+                    ]
+                }
+            ],
             'scale': scale
-        }, settings_json)
+        }, datasource_properties_json, indent=2)
 
-    if not path.exists(path.join(dataset_base_path, layer_name)):
-        makedirs(path.join(dataset_base_path, layer_name))
-
-    layer_json_path = path.join(dataset_base_path, layer_name, 'layer.json')
-    with open(layer_json_path, 'wt') as layer_json:
-        json.dump({
-            'typ': layer_type,
-            'class': dtype
-        }, layer_json)
-
-    section_json_path = path.join(
-        dataset_base_path, layer_name, 'section.json')
-    with open(section_json_path, 'wt') as section_json:
-        json.dump({
-            'bbox': (
-                (0, bbox[0]),
-                (0, bbox[1]),
-                (0, bbox[2])
-            ),
-            'resolutions': resolutions
-        }, section_json)
