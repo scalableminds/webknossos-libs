f9addbd1667beea7bb04270823c40d8f510e6a66
Valentin Pinkau <valentin.pinkau@googlemail.com>
2018-08-23 20:26:43 +0200

    remove reference

--- wkcuber/__init__.py
+++ wkcuber/__init__.py
@@ -1,4 +1,4 @@
 from .cubing import cubing
 from .downsampling import downsample_mag
 from .compress import compress_mag
-from .metadata import write_webknossos_metadata, read_metadata_for_layer
+from .metadata import write_webknossos_metadata
