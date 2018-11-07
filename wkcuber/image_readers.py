9864c3344bae33d7132e4ffbf7841c6525ac8ab5
Georg Wiese <georgwiese@gmail.com>
2018-11-07 12:01:22 +0100

    Add DM4 reader

--- wkcuber/image_readers.py
+++ wkcuber/image_readers.py
@@ -3,6 +3,7 @@ from os import path
 from PIL import Image
 
 from .vendor.dm3 import DM3
+from .vendor.dm4 import DM4File
 
 
 class PillowImageReader:
@@ -17,13 +18,18 @@ class PillowImageReader:
             return (test_img.width, test_img.height)
 
 
+def to_target_datatype(data: np.ndarray, target_dtype) -> np.ndarray:
+
+    factor = (1 + np.iinfo(data.dtype).max) / (
+            1 + np.iinfo(target_dtype).max
+    )
+    return (data / factor).astype(np.dtype(target_dtype))
+
+
 class Dm3ImageReader:
     def read_array(self, file_name, dtype):
         dm3_file = DM3(file_name)
-        factor = (1 + np.iinfo(dm3_file.imagedata.dtype).max) / (
-            1 + np.iinfo(dtype).max
-        )
-        this_layer = (dm3_file.imagedata / factor).astype(np.dtype(dtype))
+        this_layer = to_target_datatype(dm3_file.imagedata, dtype)
         this_layer = this_layer.swapaxes(0, 1)
         this_layer = this_layer.reshape(this_layer.shape + (1,))
         return this_layer
@@ -31,6 +37,56 @@ class Dm3ImageReader:
     def read_dimensions(self, file_name):
         test_img = DM3(file_name)
         return (test_img.width, test_img.height)
+
+
+class Dm4ImageReader:
+
+
+    def _read_tags(self, dm4file):
+
+        tags = dm4file.read_directory()
+        image_data_tag = tags.named_subdirs['ImageList'].unnamed_subdirs[1].named_subdirs['ImageData']
+        image_tag = image_data_tag.named_tags['Data']
+
+        return image_data_tag, image_tag
+
+
+    def _read_dimensions(self, dm4file, image_data_tag):
+
+        width = dm4file.read_tag_data(image_data_tag.named_subdirs['Dimensions'].unnamed_tags[0])
+        height = dm4file.read_tag_data(image_data_tag.named_subdirs['Dimensions'].unnamed_tags[1])
+        return width, height
+
+
+    def read_array(self, file_name, dtype):
+
+        print(1, file_name)
+
+        dm4file = DM4File.open(file_name)
+        image_data_tag, image_tag = self._read_tags(dm4file)
+        width, height = self._read_dimensions(dm4file, image_data_tag)
+
+        data = np.array(dm4file.read_tag_data(image_tag), dtype=np.uint16)
+        data = np.reshape(data, (height, width, 1))
+        data = to_target_datatype(data, dtype)
+
+        dm4file.close()
+        print(2, file_name)
+
+        return data
+
+
+    def read_dimensions(self, file_name):
+        print(3, file_name)
+
+        dm4file = DM4File.open(file_name)
+        image_data_tag, _ = self._read_tags(dm4file)
+        dimensions = self._read_dimensions(dm4file, image_data_tag)
+        dm4file.close()
+
+        print(4, file_name)
+
+        return dimensions
 
 
 class ImageReader:
@@ -42,6 +98,7 @@ class ImageReader:
             ".jpeg": PillowImageReader(),
             ".png": PillowImageReader(),
             ".dm3": Dm3ImageReader(),
+            ".dm4": Dm4ImageReader(),
         }
 
     def read_array(self, file_name, dtype):
