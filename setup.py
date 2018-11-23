2b2ae1e5ddde2135e86cbbad103b33ae825573ed
Norman Rzepka <code@normanrz.com>
2018-11-23 13:20:21 +0100

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='wkcuber',
     packages=find_packages(exclude=("tests",)),
-    version='0.1.5',
+    version='0.1.6',
     install_requires=[
         'scipy',
         'numpy',
