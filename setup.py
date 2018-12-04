1e145a43858813585122e04ee9138dc8faa26c4f
Norman Rzepka <code@normanrz.com>
2018-12-04 14:06:38 +0100

    v0.1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='wkcuber',
     packages=find_packages(exclude=("tests",)),
-    version='0.1.7',
+    version='0.1.8',
     install_requires=[
         'scipy',
         'numpy',
