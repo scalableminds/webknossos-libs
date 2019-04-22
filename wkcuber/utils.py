8caefc3cff7c9ec9f873df1e7ce9e922d4516d36
MartinBuessemeyer <martin.buessemeyer@student.hpi.de>
2019-04-22 15:56:10 +0200

    removed naming conflict in argparse

--- wkcuber/utils.py
+++ wkcuber/utils.py
@@ -51,7 +51,7 @@ def open_knossos(info):
 
 def add_verbose_flag(parser):
     parser.add_argument(
-        "--silent", "-s", help="Verbose output", dest="verbose", action="store_false"
+        "--silent", "-silent", help="Verbose output", dest="verbose", action="store_false"
     )
 
     parser.set_defaults(verbose=True)
