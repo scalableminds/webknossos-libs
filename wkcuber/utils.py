8c3df702b8e2392708ae066db81018dd93baf51e
MartinBuessemeyer <martin.buessemeyer@student.hpi.de>
2019-04-22 19:08:55 +0200

    changed helt of --silent

--- wkcuber/utils.py
+++ wkcuber/utils.py
@@ -51,7 +51,7 @@ def open_knossos(info):
 
 def add_verbose_flag(parser):
     parser.add_argument(
-        "--silent", "-silent", help="Verbose output", dest="verbose", action="store_false"
+        "--silent", "-silent", help="Silent output", dest="verbose", action="store_false"
     )
 
     parser.set_defaults(verbose=True)
