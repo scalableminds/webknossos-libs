d7f705d418c808e9ffe91d3fe120eb617edd5992
MartinBuessemeyer <martin.buessemeyer@student.hpi.de>
2019-04-30 10:40:54 +0200

    started working on the test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-from wkcuber.utils import get_chunks, get_regular_chunks
+from wkcuber.utils import get_chunks, get_regular_chunks, BufferedSliceWriter
 
 BLOCK_LEN = 32
 
@@ -27,3 +27,10 @@ def test_get_regular_chunks_max_inclusive():
     assert list(target[0]) == list(range(4, 5))
     # The last chunk should include 44
     assert list(target[-1]) == list(range(44, 45))
+
+
+def test_buffered_slice_writer():
+    test_img = np.arange(24 * 24).reshape(24, 24).astype(np.uint16)
+    bbox = {'topleft': (0, 0, 0), 'size': (24, 24, 35)}
+    origin = (0, 0, 0)
+    with BufferedSliceWriter.open('testoutput/buffered_slice_writer', 'color', np.uint16, bbox, origin) as writer:
