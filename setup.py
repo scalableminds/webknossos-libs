c14835052dd96b384a91c91584b70a63c643d5d0
Philipp Otto <philippotto@users.noreply.github.com>
2019-05-09 12:49:51 +0200

    add cluster tools to setup.py and bump version to 0.2.9 (#83)

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="wkcuber",
     packages=find_packages(exclude=("tests",)),
-    version="0.2.8",
-    install_requires=["scipy", "numpy", "pillow", "pyyaml", "wkw"],
+    version="0.2.9",
+    install_requires=["scipy", "numpy", "pillow", "pyyaml", "wkw", "cluster_tools>=1.18"],
     description="A cubing tool for webKnossos",
     author="Norman Rzepka",
     author_email="norman.rzepka@scalableminds.com",
