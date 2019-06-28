71418cd3e4fe4cc23afd4bef67e40b4e1dd96c98
Philipp Otto <philippotto@users.noreply.github.com>
2019-06-28 14:10:23 +0200

    Use find packages (#45)
    * use find_packages in setup.py
    
    * bump to v1.26

--- cluster_tools/setup.py
+++ cluster_tools/setup.py
@@ -1,12 +1,12 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def _read(fn):
     path = os.path.join(os.path.dirname(__file__), fn)
     return open(path).read()
 
 setup(name='cluster_tools',
-      version='v1.25',
+      version='v1.26',
       description='Utility library for easily distributing code execution on clusters',
       author='scalableminds',
       author_email='hello@scalableminds.com',
@@ -14,7 +14,7 @@ setup(name='cluster_tools',
       license='MIT',
       platforms='ALL',
 
-      packages=['cluster_tools'],
+      packages=find_packages(),
       install_requires=[
           'cloudpickle',
           'futures',
