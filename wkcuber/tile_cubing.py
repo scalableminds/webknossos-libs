02b58352c88f774681d2d4a54d2977fc3a3e7315
Martin Buessemeyer <Martin.Buessemeyer@student,.hpi.de>
2019-07-30 18:36:39 +0200

    finished improving the performance

--- wkcuber/tile_cubing.py
+++ wkcuber/tile_cubing.py
@@ -16,12 +16,13 @@ from wkcuber.utils import (
     get_executor_for_args,
     wait_and_ensure_success,
     setup_logging,
+    get_regular_chunks,
 )
 from wkcuber.cubing import create_parser as create_cubing_parser, read_image_file
 from wkcuber.image_readers import image_reader
 
 BLOCK_LEN = 32
-PADDING_FILE_NAME = '/'
+PADDING_FILE_NAME = "/"
 
 
 # similar to ImageJ https://imagej.net/BigStitcher_StackLoader#File_pattern
@@ -95,6 +96,8 @@ def detect_interval_for_dimensions(
     x_max = 0
     y_max = 0
     z_max = 0
+    arbitrary_file = None
+    file_count = 0
     for x in range(x_decimal_length):
         for y in range(y_decimal_length):
             for z in range(z_decimal_length):
@@ -102,7 +105,9 @@ def detect_interval_for_dimensions(
                     file_path_pattern, {"z": z, "y": y, "x": x}
                 )
                 found_files = glob(specific_pattern)
+                file_count += len(found_files)
                 for file in found_files:
+                    arbitrary_file = file
                     occurrences = re.findall("({x+}|{y+}|{z+})", file_path_pattern)
                     index_offset_caused_by_brackets_and_specific_length = 0
                     for occurrence in occurrences:
@@ -161,7 +166,7 @@ def detect_interval_for_dimensions(
                                 z_max if z_max > coordinate_value else coordinate_value
                             )
 
-    return z_min, z_max, y_min, y_max, x_min, x_max
+    return z_min, z_max, y_min, y_max, x_min, x_max, arbitrary_file, file_count
 
 
 def list_all_source_files_ordered(
@@ -219,7 +224,9 @@ def list_all_source_files_ordered(
     return ordered_files, number_of_files_found
 
 
-def pad_files_for_regular_chunk_alignment(ordered_files: List[str], z_min: int, z_max: int, chunk_size: int = BLOCK_LEN):
+def pad_files_for_regular_chunk_alignment(
+    ordered_files: List[str], z_min: int, z_max: int, chunk_size: int = BLOCK_LEN
+):
     new_z_min = floor(z_min / chunk_size) * chunk_size
     new_z_max = ceil(z_max / chunk_size) * chunk_size - 1
     number_of_pad_files_to_prepend = z_min - new_z_min
@@ -227,54 +234,98 @@ def pad_files_for_regular_chunk_alignment(ordered_files: List[str], z_min: int, 
 
     x_length = len(ordered_files[0][0])
     y_length = len(ordered_files[0])
-    invalid_z_dimension_files = [[PADDING_FILE_NAME for x in range(x_length)] for y in range(y_length)]
+    invalid_z_dimension_files = [
+        [PADDING_FILE_NAME for x in range(x_length)] for y in range(y_length)
+    ]
 
     padded_ordered_files = [invalid_z_dimension_files] * number_of_pad_files_to_prepend
     padded_ordered_files.extend(ordered_files)
-    padded_ordered_files.extend([invalid_z_dimension_files] * number_of_pad_files_to_append)
+    padded_ordered_files.extend(
+        [invalid_z_dimension_files] * number_of_pad_files_to_append
+    )
     return padded_ordered_files, new_z_min, new_z_max
+
+
+def find_file_with_dimensions(
+    file_path_pattern: str,
+    x_value: int,
+    y_value: int,
+    z_value: int,
+    x_decimal_length: int,
+    y_decimal_length: int,
+    z_decimal_length: int,
+) -> str:
+    # todo log_10 optimization
+    found_path = None
+
+    # try to find the file with all combinations of number lengths
+    for z_missing_number_length in range(x_decimal_length):
+        for y_missing_number_length in range(y_decimal_length):
+            for x_missing_number_length in range(z_decimal_length):
+                file_path = replace_coordinates(
+                    file_path_pattern,
+                    {
+                        "z": (z_value, z_missing_number_length),
+                        "y": (y_value, y_missing_number_length),
+                        "x": (x_value, x_missing_number_length),
+                    },
+                )
+                if os.path.isfile(file_path):
+                    # set the file as found and break out of the
+                    found_path = file_path
+                    break
+            if found_path:
+                break
+        if found_path:
+            break
+    return found_path
 
 
 def tile_cubing_job(
     target_wkw_info,
-    batch_ordered_files,
+    z_batches,
+    input_path_pattern,
     batch_size,
     tile_size,
-    z_offset,
-    y_offset,
-    x_offset,
+    y_min,
+    y_max,
+    x_min,
+    x_max,
+    x_decimal_length,
+    y_decimal_length,
+    z_decimal_length,
 ):
-    if len(batch_ordered_files) == 0:
+    if len(z_batches) == 0:
         return
 
     with open_wkw(target_wkw_info) as target_wkw:
         # Iterate over the z batches
         # Batching is useful to utilize IO more efficiently
-        z_batch_offset = 0
-        for z_batch in get_chunks(batch_ordered_files, batch_size):
+        for z_batch in get_chunks(z_batches, batch_size):
             try:
                 ref_time = time.time()
                 logging.info("Cubing z={}-{}".format(z_batch[0], z_batch[-1]))
 
-                for x in range(len(z_batch[0][0])):
-                    for y in range(len(z_batch[0])):
+                for x in range(x_min, x_max + 1):
+                    for y in range(y_min, y_max + 1):
                         ref_time2 = time.time()
                         buffer = []
-                        for z in range(len(z_batch)):
+                        for z in z_batch:
                             # Read file if exists or zeros instead
-                            if os.path.isfile(z_batch[z][y][x]):
-                                image = read_image_file(
-                                    z_batch[z][y][x], target_wkw_info.dtype
-                                )
+                            file = find_file_with_dimensions(
+                                input_path_pattern,
+                                x,
+                                y,
+                                z,
+                                x_decimal_length,
+                                y_decimal_length,
+                                z_decimal_length,
+                            )
+                            if file:
+                                image = read_image_file(file, target_wkw_info.dtype)
                                 image = np.squeeze(image)
                                 buffer.append(image)
                             else:
-                                # print a warning if the file is not part of the padding
-                                if not z_batch[z][y][x] == PADDING_FILE_NAME:
-                                    logging.warning(
-                                        f"File: {z_batch[z][y][x]} expected but not found. The file will be skipped. "
-                                        f"This might produce unexpected results."
-                                    )
                                 # add zeros instead
                                 buffer.append(
                                     np.squeeze(
@@ -288,102 +339,73 @@ def tile_cubing_job(
                             buffer = np.transpose(buffer, (3, 0, 1, 2))
                         # Write buffer to target if not empty
                         if np.any(buffer != 0):
-                            print((x + x_offset) * tile_size[0],
-                                    (y + y_offset) * tile_size[1],
-                                    z_offset + z_batch_offset, buffer.shape)
                             target_wkw.write(
-                                [
-                                    (x + x_offset) * tile_size[0],
-                                    (y + y_offset) * tile_size[1],
-                                    z_offset + z_batch_offset,
-                                ],
-                                buffer,
+                                [x * tile_size[0], y * tile_size[1], z_batch[0]], buffer
                             )
                         logging.debug(
                             "Cubing of z={}-{} x={} y={} took {:.8f}s".format(
-                                z_offset + z_batch_offset,
-                                z_offset + z_batch_offset + len(z_batch),
-                                x + x_offset,
-                                y + y_offset,
-                                time.time() - ref_time2,
+                                z_batch[0], z_batch[-1], x, y, time.time() - ref_time2
                             )
                         )
                 logging.debug(
                     "Cubing of z={}-{} took {:.8f}s".format(
-                        z_offset + z_batch_offset,
-                        z_offset + z_batch_offset + len(z_batch),
-                        time.time() - ref_time,
+                        z_batch[0], z_batch[-1], time.time() - ref_time
                     )
                 )
             except Exception as exc:
                 logging.error(
                     "Cubing of z={}-{} failed with {}".format(
-                        z_offset + z_batch_offset,
-                        z_offset + z_batch_offset + len(z_batch),
-                        exc,
+                        z_batch[0], z_batch[-1], exc
                     )
                 )
                 raise exc
-            z_batch_offset += len(z_batch)
 
 
 def tile_cubing(
     target_path, layer_name, dtype, batch_size, input_path_pattern, args=None
 ):
-    logging.info("Searching for files with the input pattern. This might take a while.")
     x_decimal_length, y_decimal_length, z_decimal_length = get_digit_numbers_for_dimension(
         input_path_pattern
     )
-    z_min, z_max, y_min, y_max, x_min, x_max = detect_interval_for_dimensions(
+    z_min, z_max, y_min, y_max, x_min, x_max, arbitraty_file, file_count = detect_interval_for_dimensions(
         input_path_pattern, x_decimal_length, y_decimal_length, z_decimal_length
     )
 
-    ordered_files, number_of_files_found = list_all_source_files_ordered(
-        input_path_pattern,
-        z_min,
-        z_max,
-        y_min,
-        y_max,
-        x_min,
-        x_max,
-        x_decimal_length,
-        y_decimal_length,
-        z_decimal_length,
-    )
-    if len(ordered_files) == 0:
+    if not arbitraty_file:
         logging.error("No source files found")
         return
 
     # Determine tile size from first matching file
-    tile_size = image_reader.read_dimensions(ordered_files[0][0][0])
-    num_channels = image_reader.read_channel_count(ordered_files[0][0][0])
+    tile_size = image_reader.read_dimensions(arbitraty_file)
+    num_channels = image_reader.read_channel_count(arbitraty_file)
     tile_size = (tile_size[0], tile_size[1], num_channels)
     logging.info(
-        "Found source files: count={} with tile_size={}x{} and expected to find for contiguous data count={}".format(
-            number_of_files_found,
-            tile_size[0],
-            tile_size[1],
-            len(ordered_files) * len(ordered_files[0]) * len(ordered_files[0][0]),
+        "Found source files: count={} with tile_size={}x{}".format(
+            file_count, tile_size[0], tile_size[1]
         )
     )
-    ordered_files, z_min, z_max = pad_files_for_regular_chunk_alignment(ordered_files, z_min, z_max)
 
     target_wkw_info = WkwDatasetInfo(target_path, layer_name, dtype, 1)
     ensure_wkw(target_wkw_info, num_channels=num_channels)
     with get_executor_for_args(args) as executor:
         futures = []
         # Iterate over all z batches
-        for z_start_index in range(0, len(ordered_files), BLOCK_LEN):
+        for z_batch in get_regular_chunks(z_min, z_max, BLOCK_LEN):
             futures.append(
                 executor.submit(
                     tile_cubing_job,
                     target_wkw_info,
-                    ordered_files[z_start_index : z_start_index + BLOCK_LEN],
+                    list(z_batch),
+                    input_path_pattern,
                     batch_size,
                     tile_size,
-                    z_min + z_start_index,
                     y_min,
+                    y_max,
                     x_min,
+                    x_max,
+                    x_decimal_length,
+                    y_decimal_length,
+                    z_decimal_length,
                 )
             )
         wait_and_ensure_success(futures)
