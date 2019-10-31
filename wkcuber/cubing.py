6867e0f73d5dcc83f8b33edecb87e78e0056f3b2
Philipp Otto <philipp.4096@googlemail.com>
2019-10-31 08:49:05 +0100

    set type of wkw_file_len arg

--- wkcuber/cubing.py
+++ wkcuber/cubing.py
@@ -54,6 +54,7 @@ def create_parser():
     parser.add_argument(
         "--wkw_file_len",
         default=32,
+        type=int,
         help="Amount of blocks which are written per dimension to a wkw cube. The default value of 32 means that 1024 slices are written to one cube (since one block has 32**3 voxels by default). For single-channel uint8 data, this results in 1 GB per cube file. If file_len is set to 1, only 32 slices are written to one cube. Must be a power of two.",
     )
 
