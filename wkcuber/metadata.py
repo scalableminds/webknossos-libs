64897213de8fac7a45e3ab10232240b4c608cb05
leowe <13684843+leowe@users.noreply.github.com>
2019-11-07 14:54:39 +0100

    reformatting

--- wkcuber/metadata.py
+++ wkcuber/metadata.py
@@ -250,15 +250,9 @@ def detect_standard_layer(
 
     # BB can be created manually
     # assert the presence/spelling of all attributes
-    assert (
-        "width" in bbox
-    ), "Attribute `width` is missing/misspelled in bounding box"
-    assert (
-        "height" in bbox
-    ), "Attribute `height` is missing/misspelled in bounding box"
-    assert (
-        "depth" in bbox
-    ), "Attribute `depth` is missing/misspelled in bounding box"
+    assert "width" in bbox, "Attribute `width` is missing/misspelled in bounding box"
+    assert "height" in bbox, "Attribute `height` is missing/misspelled in bounding box"
+    assert "depth" in bbox, "Attribute `depth` is missing/misspelled in bounding box"
     assert (
         "topLeft" in bbox
     ), "Attribute `topLeft` is missing/misspelled in bounding box"
