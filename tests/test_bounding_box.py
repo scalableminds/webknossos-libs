1c4abb2898432d10dbf7677d20812fede3f3c7a2
Georg Wiese <georgwiese@gmail.com>
2020-06-02 16:01:29 +0200

    Run black

--- tests/test_bounding_box.py
+++ tests/test_bounding_box.py
@@ -4,10 +4,18 @@ import pytest
 
 def test_align_with_mag():
 
-    assert BoundingBox((1, 1, 1), (10, 10, 10)).align_with_mag(Mag(2)) == BoundingBox(topleft=(0, 0, 0), size=(12, 12, 12))
-    assert BoundingBox((1, 1, 1), (9, 9, 9)).align_with_mag(Mag(2)) == BoundingBox(topleft=(0, 0, 0), size=(10, 10, 10))
-    assert BoundingBox((1, 1, 1), (9, 9, 9)).align_with_mag(Mag(4)) == BoundingBox(topleft=(0, 0, 0), size=(12, 12, 12))
-    assert BoundingBox((1, 2, 3), (9, 9, 9)).align_with_mag(Mag(2)) == BoundingBox(topleft=(0, 2, 2), size=(10, 10, 10))
+    assert BoundingBox((1, 1, 1), (10, 10, 10)).align_with_mag(Mag(2)) == BoundingBox(
+        topleft=(0, 0, 0), size=(12, 12, 12)
+    )
+    assert BoundingBox((1, 1, 1), (9, 9, 9)).align_with_mag(Mag(2)) == BoundingBox(
+        topleft=(0, 0, 0), size=(10, 10, 10)
+    )
+    assert BoundingBox((1, 1, 1), (9, 9, 9)).align_with_mag(Mag(4)) == BoundingBox(
+        topleft=(0, 0, 0), size=(12, 12, 12)
+    )
+    assert BoundingBox((1, 2, 3), (9, 9, 9)).align_with_mag(Mag(2)) == BoundingBox(
+        topleft=(0, 2, 2), size=(10, 10, 10)
+    )
 
 
 def test_in_mag():
@@ -18,4 +26,6 @@ def test_in_mag():
     with pytest.raises(AssertionError):
         BoundingBox((2, 2, 2), (9, 9, 9)).in_mag(Mag(2))
 
-    assert BoundingBox((2, 2, 2), (10, 10, 10)).in_mag(Mag(2)) == BoundingBox(topleft=(1, 1, 1), size=(5, 5, 5))
+    assert BoundingBox((2, 2, 2), (10, 10, 10)).in_mag(Mag(2)) == BoundingBox(
+        topleft=(1, 1, 1), size=(5, 5, 5)
+    )
