9a945898c05f56b4efe9c9a4266f345ad831127f
Robert S <r.schwanhold@hotmail.de>
2021-02-08 14:22:02 +0100

    reformat code

--- wkcuber/api/View.py
+++ wkcuber/api/View.py
@@ -226,7 +226,7 @@ class View:
                 size=cast(Tuple[int, int, int], tuple(target_chunk.size)),
                 relative_offset=cast(
                     Tuple[int, int, int], tuple(relative_target_offset)
-                )
+                ),
             )
 
             job_args.append((source_chunk_view, target_chunk_view, i))
