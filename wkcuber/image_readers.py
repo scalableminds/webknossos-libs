3af958b2252c996f7553019433925a41e12fc469
Youri K <youri.kaminsky@student.hpi.de>
2021-04-01 14:27:18 +0200

    support czi files

--- wkcuber/image_readers.py
+++ wkcuber/image_readers.py
@@ -8,6 +8,7 @@ from PIL import Image
 from .vendor.dm3 import DM3
 from .vendor.dm4 import DM4File, DM4TagHeader
 from tifffile import TiffFile
+from czifile import CziFile
 
 # Disable PIL's maximum image limit.
 Image.MAX_IMAGE_PIXELS = None
@@ -146,6 +147,14 @@ def find_count_of_axis(tif_file: TiffFile, axis: str) -> int:
         return tif_series.shape[index]  # pylint: disable=unsubscriptable-object
 
 
+def find_count_of_czi_axis(czi_file: CziFile, axis: str) -> int:
+    index = czi_file.axes.find(axis)
+    if index == -1:
+        return 1
+    else:
+        return czi_file.shape[index]
+
+
 class TiffImageReader(ImageReader):
     def read_array(self, file_name: str, dtype: np.dtype, z_slice: int) -> np.ndarray:
         with TiffFile(file_name) as tif_file:
@@ -199,11 +208,128 @@ class TiffImageReader(ImageReader):
             ].dtype.name
 
 
+class CziImageReader(ImageReader):
+    def __init__(self) -> None:
+        self.tile_shape = None
+
+    # returns format (X, Y)
+    def _read_array_single_channel(
+        self, czi_file: CziFile, channel_slice: int, dtype: np.dtype, z_slice: int
+    ) -> np.ndarray:
+        channel_index = czi_file.axes.find("C")
+        x_index = czi_file.axes.find("X")
+        y_index = czi_file.axes.find("Y")
+        z_index = czi_file.axes.find("Z")
+        channel_file_start = czi_file.start[channel_index]
+        z_file_start = czi_file.start[z_index]
+        for entry in czi_file.filtered_subblock_directory:
+            if (
+                entry.start[z_index] - z_file_start == z_slice
+                and entry.start[channel_index] - channel_file_start == channel_slice
+            ):
+                data = entry.data_segment().data()
+                if x_index > y_index:
+                    data = data.reshape(data.shape[y_index], data.shape[x_index])
+                    data = data.swapaxes(0, 1)
+                else:
+                    data = data.reshape(data.shape[x_index], data.shape[y_index])
+                data = to_target_datatype(data, dtype)
+                return data
+
+    # return format will be (X, Y, channel_count)
+    def _read_array_all_channels(
+        self, czi_file: CziFile, dtype: np.dtype, z_slice: int
+    ) -> np.ndarray:
+        x_index = czi_file.axes.find("X")
+        y_index = czi_file.axes.find("Y")
+        z_index = czi_file.axes.find("Z")
+        c_index = czi_file.axes.find("C")
+        indices = [(x_index, "X"), (y_index, "Y"), (c_index, "C")]
+        indices.sort()
+
+        z_start = czi_file.start[z_index]
+        for entry in czi_file.filtered_subblock_directory:
+            if entry.start[z_index] - z_start == z_slice:
+                data = entry.data_segment().data()
+                data = data.reshape(
+                    data.shape[indices[0][0]],
+                    data.shape[indices[1][0]],
+                    data.shape[indices[2][0]],
+                )
+                data = np.transpose(
+                    data,
+                    (
+                        [i for i, d in enumerate(indices) if d[1] == "X"][0],
+                        [i for i, d in enumerate(indices) if d[1] == "Y"][0],
+                        [i for i, d in enumerate(indices) if d[1] == "C"][0],
+                    ),
+                )
+                data = to_target_datatype(data, dtype)
+                return data
+
+    def read_array(self, file_name: str, dtype: np.dtype, z_slice: int) -> np.ndarray:
+        with CziFile(file_name) as czi_file:
+            channel_index = czi_file.axes.find("C")  # pylint: disable=no-member
+            x_count, y_count = self.read_dimensions(file_name)
+            channel_count = self.read_channel_count(file_name)
+            # we assume the tile shape is constant, so we can cache it
+            if self.tile_shape is None:
+                self.tile_shape = (
+                    czi_file.filtered_subblock_directory[  # pylint: disable=unsubscriptable-object
+                        0
+                    ]
+                    .data_segment()
+                    .data()
+                    .shape
+                )
+
+            assert self.tile_shape is not None, "Cannot read tile shape format."
+
+            # we assume either all channel are in one tile or each tile is single channel
+            if self.tile_shape[channel_index] != channel_count:
+                output = np.empty((channel_count, x_count, y_count), dtype)
+                # format is (channel_count, x, y)
+                for i in range(0, channel_count):
+                    output[i] = self._read_array_single_channel(
+                        czi_file, i, dtype, z_slice
+                    )
+
+                # transpose format to x, y, channel_count
+                np.transpose(output, (1, 2, 0))
+                return output
+            else:
+                return self._read_array_all_channels(czi_file, dtype, z_slice)
+
+    def read_dimensions(self, file_name: str) -> Tuple[int, int]:
+        with CziFile(file_name) as czi_file:
+            return find_count_of_czi_axis(czi_file, "X"), find_count_of_czi_axis(
+                czi_file, "Y"
+            )
+
+    def read_channel_count(self, file_name: str) -> int:
+        with CziFile(file_name) as czi_file:
+            return find_count_of_czi_axis(czi_file, "C")
+
+    def read_z_slices_per_file(self, file_name: str) -> int:
+        with CziFile(file_name) as czi_file:
+            return find_count_of_czi_axis(czi_file, "Z")
+
+    def read_dtype(self, file_name: str) -> str:
+        with CziFile(file_name) as czi_file:
+            return czi_file.dtype.name  # pylint: disable=no-member
+
+
 class ImageReaderManager:
     def __init__(self) -> None:
         self.readers: Dict[
             str,
-            Union[TiffImageReader, PillowImageReader, Dm3ImageReader, Dm4ImageReader],
+            Union[
+                TiffImageReader,
+                PillowImageReader,
+                Dm3ImageReader,
+                Dm4ImageReader,
+                CziImageReader,
+            ],
         ] = {
             ".tif": TiffImageReader(),
             ".tiff": TiffImageReader(),
@@ -212,6 +338,7 @@ class ImageReaderManager:
             ".png": PillowImageReader(),
             ".dm3": Dm3ImageReader(),
             ".dm4": Dm4ImageReader(),
+            ".czi": CziImageReader(),
         }
 
     def read_array(self, file_name: str, dtype: np.dtype, z_slice: int) -> np.ndarray:
