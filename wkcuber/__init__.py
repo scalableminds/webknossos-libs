4de365b65f5e793de0eaea71be1813911c321812
Norman Rzepka <norman.rzepka@scalableminds.com>
2021-05-02 17:15:26 +0200

    Update __init__.py
    Fixes #301

--- wkcuber/__init__.py
+++ wkcuber/__init__.py
@@ -2,3 +2,4 @@ from .cubing import cubing
 from .downsampling import downsample_mags
 from .compress import compress_mag
 from .metadata import write_webknossos_metadata
+from .api.Dataset import WKDataset
