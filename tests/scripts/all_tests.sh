2c1d09a21efebd9a983ef9091c1e268f1d920e71
Norman Rzepka <code@normanrz.com>
2021-08-23 14:03:21 +0200

    refactor tests

--- tests/scripts/all_tests.sh
+++ tests/scripts/all_tests.sh
@@ -6,18 +6,19 @@ if [ -d "./testoutput" ]; then rm -Rf ./testoutput; fi
 BASEDIR=./tests/scripts
 
 sh ${BASEDIR}/tiff_cubing.sh
+sh ${BASEDIR}/tile_cubing.sh
 sh ${BASEDIR}/simple_tiff_cubing.sh
 sh ${BASEDIR}/simple_tiff_cubing_no_compression.sh
-sh ${BASEDIR}/tile_cubing.sh
 sh ${BASEDIR}/meta_generation.sh
 sh ${BASEDIR}/knossos_conversion.sh
 
 mkdir -p testdata/tiff_mag_2_reference
 tar -xzvf testdata/tiff_mag_2_reference.tar.gz -C testdata/tiff_mag_2_reference
 
+sh ${BASEDIR}/downsampling.sh
+sh ${BASEDIR}/upsampling.sh
 sh ${BASEDIR}/anisotropic_downsampling.sh
 sh ${BASEDIR}/compression_and_verification.sh
-sh ${BASEDIR}/downsampling.sh
 sh ${BASEDIR}/in_memory_downsampled_cubing.sh
 sh ${BASEDIR}/in_place_compression.sh
 sh ${BASEDIR}/simple_anisotropic_tiff_cubing.sh
