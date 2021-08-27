35979166756e71d4bb22846c304e905c7dc757c0
Jonathan Striebel <jonathan.striebel@gmail.com>
2021-08-27 15:12:50 +0200

    use dunamai instead of setuptools_scm

--- wkcuber/publish.sh
+++ wkcuber/publish.sh
@@ -3,7 +3,7 @@ set -eEuo pipefail
 set +x
 
 cp pyproject.toml pyproject.toml.bak
-PKG_VERSION=$(python -c "from setuptools_scm import get_version;print(get_version(),end='')")
+PKG_VERSION=$(dunamai from git)
 
 sed -i "0,/version = \".*\"/{s/version = \".*\"/version = \"$PKG_VERSION\"/}" pyproject.toml
 poetry publish --build -u $PYPI_USERNAME -p $PYPI_PASSWORD
