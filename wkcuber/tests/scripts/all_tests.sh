3d0ad40850b578d288bd5e860e3abfe7b3dedfe3
Jonathan Striebel <jonathan.striebel@gmail.com>
2021-09-08 15:56:45 +0200

    Revert "test failure"
    This reverts commit 5a6fb67d6b6d65c105596c62b44ff25ff6c78916.

--- wkcuber/tests/scripts/all_tests.sh
+++ wkcuber/tests/scripts/all_tests.sh
@@ -5,8 +5,6 @@ if [ -d "./testoutput" ]; then rm -Rf ./testoutput; fi
 
 BASEDIR=./tests/scripts
 
-false
-
 mkdir -p testdata/tiff_mag_2_reference
 tar -xzvf testdata/tiff_mag_2_reference.tar.gz -C testdata/tiff_mag_2_reference
 
