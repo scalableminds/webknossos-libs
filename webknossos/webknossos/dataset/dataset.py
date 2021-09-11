d699cf3ed76441fdd821549a93102544b0d28f26
Robert S <r.schwanhold@hotmail.de>
2021-09-11 19:58:37 +0200

    format code

--- webknossos/webknossos/dataset/dataset.py
+++ webknossos/webknossos/dataset/dataset.py
@@ -385,7 +385,10 @@ class Dataset:
         self._export_as_json()
 
     def add_symlink_layer(
-        self, foreign_layer: Union[str, Path, Layer], make_relative: bool = False, new_layer_name: str = None
+        self,
+        foreign_layer: Union[str, Path, Layer],
+        make_relative: bool = False,
+        new_layer_name: str = None,
     ) -> Layer:
         """
         Creates a symlink to the data at `foreign_layer` which belongs to another dataset.
@@ -402,7 +405,9 @@ class Dataset:
             foreign_layer_path = Path(foreign_layer)
 
         foreign_layer_name = foreign_layer_path.name
-        layer_name = new_layer_name if new_layer_name is not None else foreign_layer_name
+        layer_name = (
+            new_layer_name if new_layer_name is not None else foreign_layer_name
+        )
         if layer_name in self.layers.keys():
             raise IndexError(
                 f"Cannot create symlink to {foreign_layer_path}. This dataset already has a layer called {layer_name}."
@@ -414,7 +419,9 @@ class Dataset:
             else Path(os.path.abspath(foreign_layer_path))
         )
         os.symlink(foreign_layer_symlink_path, join(self.path, layer_name))
-        original_layer = Dataset(foreign_layer_path.parent).get_layer(foreign_layer_name)
+        original_layer = Dataset(foreign_layer_path.parent).get_layer(
+            foreign_layer_name
+        )
         layer_properties = copy.deepcopy(original_layer._properties)
         layer_properties.name = layer_name
         self._properties.data_layers += [layer_properties]
@@ -425,7 +432,9 @@ class Dataset:
         self._export_as_json()
         return self.layers[layer_name]
 
-    def add_copy_layer(self, foreign_layer: Union[str, Path, Layer], new_layer_name: str = None) -> Layer:
+    def add_copy_layer(
+        self, foreign_layer: Union[str, Path, Layer], new_layer_name: str = None
+    ) -> Layer:
         """
         Copies the data at `foreign_layer` which belongs to another dataset to the current dataset.
         Additionally, the relevant information from the `datasource-properties.json` of the other dataset are copied too.
@@ -439,14 +448,18 @@ class Dataset:
 
         foreign_layer_path = Path(os.path.abspath(foreign_layer_path))
         foreign_layer_name = foreign_layer_path.name
-        layer_name = new_layer_name if new_layer_name is not None else foreign_layer_name
+        layer_name = (
+            new_layer_name if new_layer_name is not None else foreign_layer_name
+        )
         if layer_name in self.layers.keys():
             raise IndexError(
                 f"Cannot copy {foreign_layer_path}. This dataset already has a layer called {layer_name}."
             )
 
         shutil.copytree(foreign_layer_path, join(self.path, layer_name))
-        original_layer = Dataset(foreign_layer_path.parent).get_layer(foreign_layer_name)
+        original_layer = Dataset(foreign_layer_path.parent).get_layer(
+            foreign_layer_name
+        )
         layer_properties = copy.deepcopy(original_layer._properties)
         layer_properties.name = layer_name
         self._properties.data_layers += [layer_properties]
