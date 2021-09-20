c56db8eddfafa66bcc6179a893a31ea3a3dfe3aa
Jonathan Striebel <jonathan.striebel@gmail.com>
2021-09-20 16:12:48 +0200

    cleanup nml/skeleton conversion

--- webknossos/lint.sh
+++ webknossos/lint.sh
@@ -2,4 +2,5 @@
 set -eEuo pipefail
 
 pylint -j4 webknossos
+# pylint -j4 tests/**/*.py  # TODO add linting for tests
 pylint -j4 examples/*.py
--- webknossos/webknossos/skeleton/entities.py
+++ webknossos/webknossos/skeleton/entities.py
@@ -7,18 +7,14 @@ import networkx as nx
 import numpy as np
 
 import webknossos.skeleton.nml as wknml
-from webknossos.skeleton.exporter import NMLExporter
-from webknossos.skeleton.nml import NML as WkNML
-from webknossos.skeleton.nml import Group as NmlGroup
-from webknossos.skeleton.nml import Tree as NmlTree
+from webknossos.skeleton.nml.from_skeleton import from_skeleton as nml_from_skeleton
+from webknossos.skeleton.nml.to_skeleton import to_skeleton as nml_to_skeleton
 
 Vector3 = Tuple[float, float, float]
 Vector4 = Tuple[float, float, float, float]
 IntVector6 = Tuple[int, int, int, int, int, int]
 
 GroupOrGraph = Union["Group", "Graph"]
-
-nml_id_generator = itertools.count()
 
 
 @attr.define()
@@ -106,7 +102,7 @@ class Group:
 
         raise ValueError("Node id not found")
 
-    def as_nml_group(self) -> NmlGroup:
+    def as_nml_group(self) -> wknml.Group:
         return wknml.Group(
             self.id,
             self.name,
@@ -302,103 +298,10 @@ class Skeleton:
     @staticmethod
     def from_path(file_path: PathLike) -> "Skeleton":
         with open(file_path, "rb") as file_handle:
-            return Skeleton.from_nml(wknml.parse_nml(file_handle))
-
-    @staticmethod
-    def from_nml(nml: WkNML) -> "Skeleton":
-        skeleton = Skeleton(
-            name=nml.parameters.name,
-            scale=nml.parameters.scale,
-            offset=nml.parameters.offset,
-            time=nml.parameters.time,
-            edit_position=nml.parameters.editPosition,
-            edit_rotation=nml.parameters.editRotation,
-            zoom_level=nml.parameters.zoomLevel,
-            task_bounding_box=nml.parameters.taskBoundingBox,
-            user_bounding_boxes=nml.parameters.userBoundingBoxes,
-        )
-
-        groups_by_id = {}
-
-        def visit_groups(nml_groups: List[NmlGroup], current_group: Group) -> None:
-            for nml_group in nml_groups:
-                sub_group = current_group.add_group(
-                    name=nml_group.name, _enforced_id=nml_group.id
-                )
-                groups_by_id[sub_group.id] = sub_group
-                visit_groups(nml_group.children, sub_group)
-
-        visit_groups(nml.groups, skeleton.root_group)
-        for nml_tree in nml.trees:
-            if nml_tree.groupId is None:
-                new_graph = skeleton.root_group.add_graph(
-                    nml_tree.name, _enforced_id=nml_tree.id
-                )
-            else:
-                new_graph = groups_by_id[nml_tree.groupId].add_graph(
-                    nml_tree.name, _enforced_id=nml_tree.id
-                )
-            Skeleton.nml_tree_to_graph(new_graph, nml_tree)
-
-        for comment in nml.comments:
-            skeleton.get_node_by_id(comment.node).comment = comment.content
-
-        for branchpoint in nml.branchpoints:
-            node = skeleton.get_node_by_id(branchpoint.id)
-            node.is_branchpoint = True
-            if branchpoint.time != 0:
-                node.branchpoint_time = branchpoint.time
-
-        max_id = max(skeleton.get_max_graph_id(), skeleton.get_max_node_id())
-        skeleton.element_id_generator = itertools.count(max_id + 1)
-
-        return skeleton
-
-    @staticmethod
-    def nml_tree_to_graph(
-        new_graph: "Graph",
-        nml_tree: NmlTree,
-    ) -> nx.Graph:
-        """
-        A utility to convert a single wK Tree object into a [NetworkX graph object](https://networkx.org/).
-        """
-
-        optional_attribute_list = [
-            "rotation",
-            "inVp",
-            "inMag",
-            "bitDepth",
-            "interpolation",
-            "time",
-        ]
-
-        new_graph.color = nml_tree.color
-        new_graph.name = nml_tree.name
-        new_graph.group_id = nml_tree.groupId
-
-        for nml_node in nml_tree.nodes:
-            node_id = nml_node.id
-            current_node = new_graph.add_node(
-                position=nml_node.position,
-                _enforced_id=node_id,
-                radius=nml_node.radius,
-            )
-
-            for optional_attribute in optional_attribute_list:
-                if getattr(nml_node, optional_attribute) is not None:
-                    setattr(
-                        current_node,
-                        optional_attribute,
-                        getattr(nml_node, optional_attribute),
-                    )
-
-        for edge in nml_tree.edges:
-            new_graph.add_edge(edge.source, edge.target)
-
-        return new_graph
+            return nml_to_skeleton(wknml.parse_nml(file_handle))
 
     def write(self, out_path: PathLike) -> None:
-        nml = NMLExporter.generate_nml(
+        nml = nml_from_skeleton(
             self.root_group,
             self._get_nml_parameters(),
         )
