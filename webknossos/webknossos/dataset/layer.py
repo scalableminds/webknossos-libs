878a282f948d9a21947707ac143de61b3a0d6ec8
R Schwanhold <r.schwanhold@hotmail.de>
2021-11-04 12:57:23 +0100

    Type layer_category as Literal everywhere (#454)
    * introduce literal
    
    * fix import and properties
    
    * remove LayerCategories
    
    * fix imports
    
    * Update webknossos/Changelog.md
    
    Co-authored-by: Jonathan Striebel <jstriebel@users.noreply.github.com>
    
    * rename layer categories
    
    * Update test_dataset.py
    
    Co-authored-by: Jonathan Striebel <jstriebel@users.noreply.github.com>

--- webknossos/webknossos/dataset/layer.py
+++ webknossos/webknossos/dataset/layer.py
@@ -26,6 +26,7 @@ from .downsampling_utils import (
     downsample_cube_job,
     parse_interpolation_mode,
 )
+from .layer_categories import COLOR_CATEGORY, SEGMENTATION_CATEGORY, LayerCategoryType
 from .properties import (
     LayerProperties,
     LayerViewConfiguration,
@@ -862,8 +863,8 @@ class Layer:
         )
 
     @property
-    def category(self) -> str:
-        return LayerCategories.COLOR_TYPE
+    def category(self) -> LayerCategoryType:
+        return COLOR_CATEGORY
 
     @property
     def dtype_per_layer(self) -> str:
@@ -886,22 +887,5 @@ class SegmentationLayer(Layer):
         self.dataset._export_as_json()
 
     @property
-    def category(self) -> str:
-        return LayerCategories.SEGMENTATION_TYPE
-
-
-class LayerCategories:
-    """
-    There are two different types of layers.
-    This class can be used to specify the type of a layer during creation:
-    ```python
-    from webknossos.dataset.dataset import Dataset
-
-    dataset = Dataset(<path_to_dataset>)
-    # Adds a new layer
-    layer = dataset.add_layer("color", LayerCategories.COLOR_TYPE)
-    ```
-    """
-
-    COLOR_TYPE = "color"
-    SEGMENTATION_TYPE = "segmentation"
+    def category(self) -> LayerCategoryType:
+        return SEGMENTATION_CATEGORY
---
+++ wkcuber/wkcuber/api/layer_categories.py
@@ -0,0 +1,1 @@
+from webknossos.dataset.layer_categories import *  # pylint: disable=unused-wildcard-import,wildcard-import
