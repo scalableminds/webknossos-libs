1c37cbd6020a26a8bfa5c452f20bd310f78e66ce
Tom Herold <tom.herold@scalableminds.com>
2021-11-22 16:47:15 +0100

    updated example with more comments etc

--- webknossos/examples/upload_image_data.py
+++ webknossos/examples/upload_image_data.py
@@ -1,21 +1,34 @@
 from time import gmtime, strftime
-
 from skimage import data
+import numpy as np
 
 import webknossos as wk
+from webknossos.dataset import COLOR_CATEGORY
 
-with wk.webknossos_context(url="http://localhost:9000", token="secretScmBoyToken"):
-    img = data.cell()
+# Get your auth token from https://webknossos.org/auth/token
+with wk.webknossos_context(url="http://localhost:9000", token="secretToken"):
+
+    # load your data - we use a an example 3D dataset here
+    img = data.cells3d()  # (z, c, y, x)
+
+    # make sure that the dimension of your data has the right order
+    # we expect the following dimensions Channels, X, Y, Z.
+    img = np.transpose(img, [1, 3, 2, 0])
+
+    # choose a name for our dataset
     time_str = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
     name = f"cell_{time_str}"
-    ds = wk.Dataset.create(name, scale=(107, 107, 107))
+
+    # scale is defined in nm
+    ds = wk.Dataset.create(name, scale=(290, 260, 260))
     layer = ds.add_layer(
-        "color",
-        "color",
+        "raw_microscopy_layer",
+        COLOR_CATEGORY,
         dtype_per_layer=img.dtype,
     )
-    # add channel and z dimensions and put X before Y,
-    # resulting dimensions are C, X, Y, Z.
-    layer.add_mag(1, compress=True).write(img.T[None, :, :, None])
+
+    layer.add_mag(1, compress=True)
+    layer.write(img)
+
     url = ds.upload()
     print(f"Successfully uploaded {url}")
