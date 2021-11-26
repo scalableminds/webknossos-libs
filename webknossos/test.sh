ef91a40c30302a69e2da16665a0d9efaa7254510
Florian M <florian@scm.io>
2021-11-26 11:26:08 +0100

    update test.sh for branch, fix docker-compose params

--- webknossos/test.sh
+++ webknossos/test.sh
@@ -4,13 +4,15 @@ set -eEuo pipefail
 if [ $# -eq 1 ] && [ "$1" = "--refresh-snapshots" ]; then
     if ! curl -sf localhost:9000/api/health; then
         WK_DOCKER_DIR="tests/.webknossos-server"
-        if [ ! -d "$WK_DOCKER_DIR" ]; then
-            git clone git@github.com:scalableminds/webknossos.git $WK_DOCKER_DIR --depth 1
-            cp tests/wk-docker-compose.override.yml $WK_DOCKER_DIR/docker-compose.override.yml
-        fi
+
+#        git clone git@github.com:scalableminds/webknossos.git $WK_DOCKER_DIR
+        pushd $WK_DOCKER_DIR
+        git checkout robuster-uploads
+        popd
+        sed -i -e 's/webKnossos.sampleOrganization.enabled=false/webKnossos.sampleOrganization.enabled=true/g' docker-compose.yml
         pushd $WK_DOCKER_DIR > /dev/null
         mkdir -p binaryData
-        export DOCKER_TAG=21.09.0
+        export DOCKER_TAG=robuster_uploads__16085
         docker-compose pull webknossos
         USER_UID=$(id -u) USER_GID=$(id -g) docker-compose up -d --no-build webknossos
         popd > /dev/null
