163926f76349e25c3ef8f5f82e010cd7bc50c91f
Norman Rzepka <code@normanrz.com>
2022-01-07 11:54:22 +0100

    use time_start/stop in compress_utils

--- webknossos/webknossos/dataset/compress_utils.py
+++ webknossos/webknossos/dataset/compress_utils.py
@@ -1,17 +1,16 @@
 import logging
-import time
 from os import makedirs, path
 from pathlib import Path
 from typing import Tuple
 
 import wkw
+from ..utils import time_start, time_stop
 
 
 def compress_file_job(args: Tuple[Path, Path]) -> None:
     source_path, target_path = args
     try:
-        logging.debug("Compressing '{}' to '{}'".format(source_path, target_path))
-        ref_time = time.time()
+        time_start("Compressing '{}' to '{}'".format(source_path, target_path))
 
         makedirs(path.dirname(target_path), exist_ok=True)
         wkw.File.compress(str(source_path), str(target_path))
@@ -19,11 +18,7 @@ def compress_file_job(args: Tuple[Path, Path]) -> None:
         if not path.exists(target_path):
             raise Exception("Did not create compressed file {}".format(target_path))
 
-        logging.debug(
-            "Compressing of '{}' took {:.8f}s".format(
-                source_path, time.time() - ref_time
-            )
-        )
+        time_stop("Compressing '{}' to '{}'".format(source_path, target_path))
 
     except Exception as exc:
         logging.error(
