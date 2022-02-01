db7d2029721714b5458586dcf2434cb7baac63b5
Florian M <florian@scm.io>
2022-02-01 10:30:23 +0100

    create tasks from files

--- webknossos/examples/task.py
+++ webknossos/examples/task.py
@@ -1,12 +1,19 @@
-from webknossos.administration import Project
+from webknossos.administration import Project, Task
+from webknossos.annotation import Annotation
 
 
 def main() -> None:
-    p = Project.get_by_name("sampleProject")
-    tasks = p.get_tasks()
+    # p = Project.get_by_name("sampleProject")
+    # tasks = p.get_tasks()
+    # print(tasks)
+    # annotations = [t.get_annotation_infos() for t in tasks]
+    # print(annotations)
+    a = Annotation.load("/home/f/scm/nml/file1.nml.zip")
+    task_type_id = "61efb258f901004c02d637ee"  # from wk web interface
+    tasks = Task.create_from_annotations(
+        "61efb258f901004c02d637ee", "sampleExp", 1, 1, "sampleProject", None, None, [a]
+    )
     print(tasks)
-    annotations = [t.get_annotation_infos() for t in tasks]
-    print(annotations)
 
 
 if __name__ == "__main__":
--- webknossos/webknossos/administration/task.py
+++ webknossos/webknossos/administration/task.py
@@ -1,6 +1,9 @@
-from typing import TYPE_CHECKING, List, Optional, Union
+import json
+import logging
+from typing import TYPE_CHECKING, Dict, List, Optional, Union
 
 import attr
+import httpx
 
 from webknossos.administration import Project
 from webknossos.annotation import Annotation, AnnotationInfo
@@ -8,8 +11,10 @@ from webknossos.client._generated.api.default import (
     annotation_infos_by_task_id,
     task_info,
 )
-from webknossos.client.context import _get_context, _get_generated_client
+from webknossos.client.context import _get_generated_client
 from webknossos.geometry import BoundingBox
+
+logger = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
     from webknossos.client._generated.models.task_info_response_200 import (
@@ -44,9 +49,74 @@ class Task:
         script_id: Optional[str],
         bounding_box: Optional[BoundingBox],
         base_annotations: List[Annotation],
-    ) -> "Task":
-        context = _get_context()
-        token = context.required_token
+    ) -> List["Task"]:
+        assert (
+            len(base_annotations) > 0
+        ), "Must supply at least one base annotation to create tasks"
+        client = _get_generated_client()
+        url = f"{client.base_url}/api/tasks/createFromFiles"
+        task_parameters = {
+            "taskTypeId": task_type_id,
+            "neededExperience": {
+                "domain": needed_experience_domain,
+                "value": needed_experience_value,
+            },
+            "openInstances": instances,
+            "projectName": project_name,
+            "scriptId": script_id,
+            "boundingBox": bounding_box.to_wkw_dict()
+            if bounding_box is not None
+            else None,
+        }
+        form_data = {"formJSON": json.dumps(task_parameters)}
+        files = {a._nml_file.path: open(a.file, "rb") for a in base_annotations}
+        print(files)
+        response = httpx.post(
+            url=url,
+            headers=client.get_headers(),
+            cookies=client.get_cookies(),
+            timeout=client.get_timeout(),
+            data=form_data,
+            files=files,
+        )
+        assert (
+            response.status_code == 200
+        ), f"Failed to create tasks from files: {response.status_code}: {response.content.decode('utf-8')}"
+        result = json.loads(response.content)
+        if "warnings" in result:
+            warnings = result["warnings"]
+            if len(warnings) > 0:
+                logger.warning(
+                    f"There were {len(warnings)} warnings during task creation:"
+                )
+            for warning in warnings:
+                logger.warning(warning)
+        assert "tasks" in result, "Invalid result of task creation"
+        successes = []
+        errors = []
+        for t in result["tasks"]:
+            print(t)
+            if "success" in t:
+                successes.append(t["success"])
+            if "error" in t:
+                errors.append(t["error"])
+        if len(errors) > 0:
+            logger.error(f"{len(errors)} tasks could not be created:")
+            for error in errors:
+                logger.error(error)
+        if len(successes) > 0:
+            logger.info(f"{len(successes)} tasks were successfully created.")
+        return [cls._from_dict(t) for t in successes]
+
+    @classmethod
+    def _from_dict(cls, response_dict: Dict) -> "Task":
+        from webknossos.client._generated.models.task_info_response_200 import (
+            TaskInfoResponse200,
+        )
+
+        return cls._from_generated_response(
+            TaskInfoResponse200.from_dict(response_dict)
+        )
 
     @classmethod
     def _from_generated_response(
