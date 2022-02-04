fbd741bbec37d4797eb932970245bcc6e0559069
Jonathan Striebel <jonathan.striebel@gmail.com>
2022-02-04 14:42:56 +0100

    undo remove_layer

--- webknossos/webknossos/dataset/dataset.py
+++ webknossos/webknossos/dataset/dataset.py
@@ -8,7 +8,6 @@ from contextlib import nullcontext
 from os import PathLike, makedirs
 from os.path import basename, join, normpath
 from pathlib import Path
-from shutil import rmtree
 from typing import (
     TYPE_CHECKING,
     Any,
@@ -503,26 +502,6 @@ class Dataset:
         layer.bounding_box = infer_bounding_box_existing_files(min_mag_view)
         return layer
 
-    def remove_layer(self, layer_name: str, remove_files: bool = False) -> None:
-        if layer_name not in self.layers.keys():
-            raise IndexError(
-                f"Removing layer {layer_name} failed. There is no layer with this name."
-            )
-
-        self._properties.data_layers = [
-            i for i in self._properties.data_layers if i.name != layer_name
-        ]
-        self._export_as_json()
-        if remove_files:
-            layer_path = self.layers[layer_name].path
-            if layer_path.exists:
-                if layer_path.is_symlink():
-                    layer_path.unlink()
-                else:
-                    # rmtree does not recurse into linked dirs, but removes the link
-                    shutil.rmtree(layer_path)
-        del self._layers[layer_name]
-
     def get_segmentation_layer(self) -> SegmentationLayer:
         """
         Returns the only segmentation layer. Prefer `get_segmentation_layers`,
@@ -585,7 +564,12 @@ class Dataset:
             layer for layer in self._properties.data_layers if layer.name != layer_name
         ]
         # delete files on disk
-        rmtree(join(self.path, layer_name))
+        layer_path = self.layers[layer_name].path
+        if layer_path.is_symlink():
+            layer_path.unlink()
+        else:
+            # rmtree does not recurse into linked dirs, but removes the link
+            shutil.rmtree(layer_path)
         self._export_as_json()
 
     def add_symlink_layer(
