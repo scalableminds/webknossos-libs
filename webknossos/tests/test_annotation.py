7a4a5dc93408932d7fd450979db1cb423d1477e8
Philipp Otto <philipp.4096@googlemail.com>
2022-02-08 16:15:28 +0100

    add test case for reading from multiple volume layers

--- webknossos/tests/test_annotation.py
+++ webknossos/tests/test_annotation.py
@@ -37,6 +37,58 @@ def test_annotation_from_file() -> None:
 
 
 @pytest.mark.vcr()
+def test_annotation_from_file_with_multi_volume() -> None:
+    annotation = wk.Annotation(
+        TESTDATA_DIR / "annotations" / "multi_volume_example_CREMI.zip"
+    )
+
+    volume_names = sorted(list(annotation.get_volume_layer_names()))
+
+    assert volume_names == ["Volume", "Volume_2"]
+
+    with annotation.temporary_volume_annotation_layer_copy(
+        source_volume_name=volume_names[0]
+    ) as layer:
+        read_voxel = layer.get_best_mag().read(
+            absolute_offset=(590, 512, 16),
+            size=(1, 1, 1),
+        )
+        assert (
+            read_voxel == 7718
+        ), f"Expected to see voxel id 7718, but saw {read_voxel} instead."
+
+        read_voxel = layer.get_best_mag().read(
+            absolute_offset=(490, 512, 16),
+            size=(1, 1, 1),
+        )
+        # When viewing the annotation online, this segment id will be 284.
+        # However, this is fallback data which is not included in this annotation.
+        # Therefore, we expect to read a 0 here.
+        assert (
+            read_voxel == 0
+        ), f"Expected to see voxel id 0, but saw {read_voxel} instead."
+
+    with annotation.temporary_volume_annotation_layer_copy(
+        source_volume_name=volume_names[1]
+    ) as layer:
+        read_voxel = layer.get_best_mag().read(
+            absolute_offset=(590, 512, 16),
+            size=(1, 1, 1),
+        )
+        assert (
+            read_voxel == 1
+        ), f"Expected to see voxel id 1, but saw {read_voxel} instead."
+
+        read_voxel = layer.get_best_mag().read(
+            absolute_offset=(490, 512, 16),
+            size=(1, 1, 1),
+        )
+        assert (
+            read_voxel == 0
+        ), f"Expected to see voxel id 0, but saw {read_voxel} instead."
+
+
+@pytest.mark.vcr()
 def test_annotation_from_url() -> None:
 
     annotation = wk.Annotation.download(
