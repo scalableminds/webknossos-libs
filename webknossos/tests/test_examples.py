dcc784a5320fdcfb495c86790be33130fea40f28
Norman Rzepka <code@normanrz.com>
2022-02-11 09:25:01 +0100

    ignore apply-mapping

--- webknossos/tests/test_examples.py
+++ webknossos/tests/test_examples.py
@@ -56,7 +56,8 @@ def test_dataset_usage() -> None:
     assert data_in_mag2_subset.shape == (3, 256, 256, 16)
 
 
-@pytest.mark.block_network(allowed_hosts=[".*"])
+@pytest.mark.block_network(allowed_hosts=["webknossos.org"])
+@pytest.mark.vcr(ignore_hosts=["webknossos.org"])
 def test_apply_merger_mode() -> None:
     import examples.apply_merger_mode as example
 
