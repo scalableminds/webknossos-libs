b0e29f449997c2a14efa4351ad1e3ea44ad00ad8
Philipp Otto <philipp.4096@googlemail.com>
2022-03-16 16:27:04 +0100

    remove unused _children property

--- webknossos/webknossos/skeleton/skeleton.py
+++ webknossos/webknossos/skeleton/skeleton.py
@@ -47,12 +47,6 @@ class Skeleton(Group):
     _enforced_id: Optional[int] = attr.field(default=None, eq=False, repr=False)
     name: str = attr.field(default="Root", init=False, eq=False, repr=False)
 
-    """Should not be used with `Skeleton`, this attribute is only useful for sub-groups. Set to `Root`."""
-    _children: Set[GroupOrGraph] = attr.ib(
-        factory=set,
-        init=False,
-        repr=lambda children: f"<{unit_len(children, 'children')}>",
-    )
     # initialized in post_init:
     _id: int = attr.field(init=False, repr=False)
     _element_id_generator: Iterator[int] = attr.field(init=False, eq=False, repr=False)
