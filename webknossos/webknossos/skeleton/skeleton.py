ffd4c1c0121be3427e56731aa8b4406df974cbe4
Philipp Otto <philipp.4096@googlemail.com>
2022-03-17 14:46:23 +0100

    fix linting

--- webknossos/webknossos/skeleton/skeleton.py
+++ webknossos/webknossos/skeleton/skeleton.py
@@ -1,10 +1,9 @@
 import itertools
 from os import PathLike
 from pathlib import Path
-from typing import Iterator, Optional, Set, Tuple, Union
+from typing import Iterator, Optional, Tuple, Union
 
 import attr
-from boltons.strutils import unit_len
 
 from webknossos.utils import warn_deprecated
 
