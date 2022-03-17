9c13052cc5b32245df0e0817f6216b1449c3b4a3
Philipp Otto <philipp.4096@googlemail.com>
2022-03-17 14:21:51 +0100

    remove isort from wkcuber for now

--- wkcuber/format.sh
+++ wkcuber/format.sh
@@ -3,8 +3,6 @@ set -eEuo pipefail
 
 if [ $# -eq 1 ] && [ "$1" = "check" ]; then
     poetry run black --check .
-    poetry run isort --check-only .
 else
-    poetry run isort .
     poetry run black .
 fi
