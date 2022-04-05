c623d2a3f9391ac74aef45a37c0c20268783f6ad
Norman Rzepka <code@normanrz.com>
2022-04-05 20:44:29 +0200

    utils

---
+++ wkcuber/wkcuber/utils.py
@@ -0,0 +1,8 @@
+from warnings import warn
+
+from ._internal.utils import *  # pylint: disable=unused-wildcard-import,wildcard-import
+
+warn(
+    "[DEPRECATION] Using `wkcuber.utils` is deprecated. "
+    "Please use the high-level APIs of the `webknossos` package instead."
+)
