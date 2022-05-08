15f407aede46376098ac363d5296cb62ff28efe7
Norman Rzepka <code@normanrz.com>
2022-05-08 20:39:33 +0200

    use remotedataset in learned_segmenter example

--- webknossos/examples/learned_segmenter.py
+++ webknossos/examples/learned_segmenter.py
@@ -1,5 +1,6 @@
 import os
 from functools import partial
+from tempfile import TemporaryDirectory
 from time import gmtime, strftime
 
 import numpy as np
@@ -22,18 +23,18 @@ def main() -> None:
     training_data_bbox = annotation.user_bounding_boxes[0]  # type: ignore[index]
     time_str = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
     new_dataset_name = annotation.dataset_name + f"_segmented_{time_str}"
-    dataset = wk.Dataset.download(
+    mag = wk.Mag(1)
+    dataset = wk.Dataset.open_remote(
         annotation.dataset_name,
         organization_id="scalable_minds",
-        path=new_dataset_name,
         webknossos_url="https://webknossos.org",
     )
-    dataset.name = new_dataset_name
 
-    volume_annotation = annotation.export_volume_layer_to_dataset(dataset)
-    volume_annotation.bounding_box = training_data_bbox
+    with annotation.temporary_volume_layer_copy() as volume_annotation_layer:
+        volume_annotation_data = volume_annotation_layer.mags[mag].read(
+            absolute_bounding_box=training_data_bbox
+        )
 
-    mag = wk.Mag(1)
     mag_view = dataset.layers["color"].mags[mag]
 
     # Step 2: Initialize a machine learning model to segment our dataset
@@ -50,7 +51,7 @@ def main() -> None:
     )  # wk data has dimensions (Channels, X, Y, Z)
     # move channels to last dimension, remove z dimension to match skimage's shape
     X_train = np.moveaxis(np.squeeze(img_data_train), 0, -1)
-    Y_train = np.squeeze(volume_annotation.mags[mag].read())
+    Y_train = np.squeeze(volume_annotation_data)
 
     segmenter.fit(X_train, Y_train)
 
@@ -64,29 +65,33 @@ def main() -> None:
     segmentation = segmentation.astype("uint8")
 
     # Step 5: Bundle everying a webKnossos layer and upload to wK for viewing and further work
-    segmentation_layer = dataset.add_layer(
-        "segmentation",
-        wk.SEGMENTATION_CATEGORY,
-        segmentation.dtype,
-        compressed=True,
-        largest_segment_id=int(segmentation.max()),
-    )
-    segmentation_layer.bounding_box = dataset.layers["color"].bounding_box
-    segmentation_layer.add_mag(mag, compress=True).write(segmentation)
+    with TemporaryDirectory() as tempdir:
+        new_dataset = wk.Dataset(
+            tempdir, voxel_size=dataset.voxel_size, name=new_dataset_name
+        )
+        segmentation_layer = new_dataset.add_layer(
+            "segmentation",
+            wk.SEGMENTATION_CATEGORY,
+            segmentation.dtype,
+            compressed=True,
+            largest_segment_id=int(segmentation.max()),
+        )
+        segmentation_layer.bounding_box = dataset.layers["color"].bounding_box
+        segmentation_layer.add_mag(mag, compress=True).write(segmentation)
 
-    remote_ds = dataset.upload(
-        layers_to_link=[
-            wk.LayerToLink(
-                organization_id="scalable_minds",
-                dataset_name=annotation.dataset_name,
-                layer_name="color",
-            )
-        ]
-        if "PYTEST_CURRENT_TEST" not in os.environ
-        else None
-    )
+        uploaded_dataset = new_dataset.upload(
+            layers_to_link=[
+                wk.LayerToLink(
+                    organization_id="scalable_minds",
+                    dataset_name=annotation.dataset_name,
+                    layer_name="color",
+                )
+            ]
+            if "PYTEST_CURRENT_TEST" not in os.environ
+            else None
+        )
 
-    url = remote_ds.url
+    url = uploaded_dataset.url
     print(f"Successfully uploaded {url}")
 
 
