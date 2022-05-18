ccaaaf3e85d5081ba8a624bcc31b852a3701255c
Jonathan Striebel <jstriebel@users.noreply.github.com>
2022-05-18 09:54:17 +0200

    Update test_context.py

--- webknossos/tests/client/test_context.py
+++ webknossos/tests/client/test_context.py
@@ -1,6 +1,6 @@
 import pytest
 
-from webknossos.client.context import _get_context, _WebknossosContext
+from webknossos.client.context import _get_context, _WebknossosContext, webknossos_context
 
 pytestmark = [pytest.mark.with_vcr]
 
@@ -15,3 +15,8 @@ def env_context() -> _WebknossosContext:
 
 def test_user_organization(env_context: _WebknossosContext) -> None:
     assert env_context.organization_id == "Organization_X"
+
+
+def test_trailing_slash_in_url(env_context: _WebknossosContext) -> None:
+    with webknossos_context(url=env_context + "/"):
+        assert env_context.url == _get_context().url
