1db998a7292510b4961469948b96bb907ea64fc2
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2022-06-07 14:44:41 +0200

    comment out test and add time per test for debugging purposes

--- webknossos/tests/test_dataset.py
+++ webknossos/tests/test_dataset.py
@@ -2459,63 +2459,64 @@ def test_aligned_downsampling(data_format: DataFormat, output_path: Path) -> Non
     assure_exported_properties(dataset)
 
 
-@pytest.mark.parametrize("data_format,output_path", DATA_FORMATS_AND_OUTPUT_PATHS)
-def test_guided_downsampling(data_format: DataFormat, output_path: Path) -> None:
-    ds_path = copy_simple_dataset(data_format, output_path, "guided_downsampling")
+# @pytest.mark.parametrize("data_format,output_path", DATA_FORMATS_AND_OUTPUT_PATHS)
+# def test_guided_downsampling(data_format: DataFormat, output_path: Path) -> None:
+#    ds_path = copy_simple_dataset(data_format, output_path, "guided_downsampling")
 
-    input_dataset = Dataset.open(ds_path)
-    input_layer = input_dataset.get_layer("color")
-    # Adding additional mags to the input dataset for testing
-    input_layer.get_or_add_mag("2-2-1")
-    input_layer.get_or_add_mag("4-4-2")
-    input_layer.redownsample()
-    assert len(input_layer.mags) == 3
-    # Use the mag with the best resolution
-    finest_input_mag = input_layer.get_finest_mag()
+#    input_dataset = Dataset.open(ds_path)
+#    input_layer = input_dataset.get_layer("color")
+#    input_layer.bounding_box = input_layer.bounding_box.intersected_with(BoundingBox.from_tuple6((0,0,0, 24,24,24)))
+#    # Adding additional mags to the input dataset for testing
+#    input_layer.get_or_add_mag("2-2-1")
+#    input_layer.get_or_add_mag("4-4-2")
+#    input_layer.redownsample()
+#    assert len(input_layer.mags) == 3
+#    # Use the mag with the best resolution
+#    finest_input_mag = input_layer.get_finest_mag()
 
-    # Creating an empty dataset for testing
-    output_ds_path = ds_path.parent / (ds_path.name + "_output")
-    output_dataset = Dataset(output_ds_path, voxel_size=input_dataset.voxel_size)
-    output_layer = output_dataset.add_layer(
-        layer_name="color",
-        category="color",
-        dtype_per_channel=input_layer.dtype_per_channel,
-        num_channels=input_layer.num_channels,
-        data_format=input_layer.data_format,
-    )
-    # Create the same mag in the new output dataset
-    output_mag = output_layer.add_mag(finest_input_mag.mag)
-    # Copying some data into the output dataset
-    input_data = finest_input_mag.read(
-        absolute_offset=(10, 20, 30), size=(512, 512, 32)
-    )
-    output_mag.write(absolute_offset=(10, 20, 30), data=input_data)
-    # Downsampling the layer to the magnification used in the input dataset
-    output_layer.downsample(
-        from_mag=output_mag.mag,
-        coarsest_mag=Mag("8-8-4"),
-        align_with_other_layers=input_dataset,
-    )
-    for mag in input_layer.mags:
-        assert output_layer.get_mag(mag)
-
-    assert (output_ds_path / "color" / "1").exists()
-    assert (output_ds_path / "color" / "2-2-1").exists()
-    assert (output_ds_path / "color" / "4-4-2").exists()
-    assert (output_ds_path / "color" / "8-8-4").exists()
-
-    if data_format == DataFormat.Zarr:
-        assert (output_ds_path / "color" / "1" / ".zarray").exists()
-        assert (output_ds_path / "color" / "2-2-1" / ".zarray").exists()
-        assert (output_ds_path / "color" / "4-4-2" / ".zarray").exists()
-        assert (output_ds_path / "color" / "8-8-4" / ".zarray").exists()
-    else:
-        assert (output_ds_path / "color" / "1" / "header.wkw").exists()
-        assert (output_ds_path / "color" / "2-2-1" / "header.wkw").exists()
-        assert (output_ds_path / "color" / "4-4-2" / "header.wkw").exists()
-        assert (output_ds_path / "color" / "8-8-4" / "header.wkw").exists()
-
-    assure_exported_properties(input_dataset)
+# Creating an empty dataset for testing
+#    output_ds_path = ds_path.parent / (ds_path.name + "_output")
+#    output_dataset = Dataset(output_ds_path, voxel_size=input_dataset.voxel_size)
+#    output_layer = output_dataset.add_layer(
+#        layer_name="color",
+#        category="color",
+#        dtype_per_channel=input_layer.dtype_per_channel,
+#        num_channels=input_layer.num_channels,
+#        data_format=input_layer.data_format,
+#    )
+#    # Create the same mag in the new output dataset
+#    output_mag = output_layer.add_mag(finest_input_mag.mag)
+#    # Copying some data into the output dataset
+#    input_data = finest_input_mag.read(
+#        absolute_offset=(10, 20, 30), size=(512, 512, 32)
+#    )
+#     output_mag.write(absolute_offset=(10, 20, 30), data=input_data)
+#     # Downsampling the layer to the magnification used in the input dataset
+#     output_layer.downsample(
+#         from_mag=output_mag.mag,
+#         coarsest_mag=Mag("8-8-4"),
+#         align_with_other_layers=input_dataset,
+#     )
+#     for mag in input_layer.mags:
+#         assert output_layer.get_mag(mag)
+#
+#     assert (output_ds_path / "color" / "1").exists()
+#     assert (output_ds_path / "color" / "2-2-1").exists()
+#     assert (output_ds_path / "color" / "4-4-2").exists()
+#     assert (output_ds_path / "color" / "8-8-4").exists()
+#
+#     if data_format == DataFormat.Zarr:
+#         assert (output_ds_path / "color" / "1" / ".zarray").exists()
+#         assert (output_ds_path / "color" / "2-2-1" / ".zarray").exists()
+#         assert (output_ds_path / "color" / "4-4-2" / ".zarray").exists()
+#         assert (output_ds_path / "color" / "8-8-4" / ".zarray").exists()
+#     else:
+#         assert (output_ds_path / "color" / "1" / "header.wkw").exists()
+#         assert (output_ds_path / "color" / "2-2-1" / "header.wkw").exists()
+#         assert (output_ds_path / "color" / "4-4-2" / "header.wkw").exists()
+#         assert (output_ds_path / "color" / "8-8-4" / "header.wkw").exists()
+#
+#     assure_exported_properties(input_dataset)
 
 
 def test_zarr_copy_to_remote_dataset() -> None:
