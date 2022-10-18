a191a1d909d5e6c37123d10671b52b453f55fa90
Norman Rzepka <code@normanrz.com>
2022-10-18 15:07:33 +0200

    explicit segment_ids

--- webknossos/examples/download_segments.py
+++ webknossos/examples/download_segments.py
@@ -4,23 +4,21 @@ from tifffile import imwrite
 import webknossos as wk
 
 ANNOTATION_ID = "634e8fe1010000b4006f3cf4"
+SEGMENT_IDS = [32, 667325]
 MAG = wk.Mag("4-4-1")
 
 
 def main() -> None:
-    annotation = wk.Annotation.download(
-        ANNOTATION_ID, webknossos_url="https://webknossos.org", skip_volume_data=True
+    dataset = wk.Annotation.open_as_remote_dataset(
+        ANNOTATION_ID, webknossos_url="https://webknossos.org"
     )
-    dataset = annotation.get_remote_annotation_dataset()
     mag = dataset.get_segmentation_layers()[0].get_mag(MAG)
-
-    segment_ids = list(annotation.get_volume_layer_segments().keys())
 
     z = mag.layer.bounding_box.topleft.z
     with mag.get_buffered_slice_reader() as reader:
         for slice_data in reader:
             slice_data = slice_data[0]  # First channel only
-            for segment_id in segment_ids:
+            for segment_id in SEGMENT_IDS:
                 segment_mask = (slice_data == segment_id).astype(
                     np.uint8
                 ) * 255  # Make a binary mask 0=empty, 255=segment
