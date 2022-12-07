699e36e62c98f23bf60e1c13fca4d81558b18bb2
Jonathan Striebel <jonathan.striebel@gmail.com>
2022-12-07 10:31:42 +0100

    use urlparse

--- webknossos/webknossos/client/_resolve_short_link.py
+++ webknossos/webknossos/client/_resolve_short_link.py
@@ -1,19 +1,29 @@
+import logging
 import re
+from urllib.parse import urlparse
 
 from webknossos.client._generated.api.default import short_link_by_key
 from webknossos.client.context import _get_generated_client, webknossos_context
 
-_SHORT_LINK_REGEX = re.compile(
-    r"^(?P<webknossos_url>https?://.*)/links/" + r"(?P<short_link_key>.*)"
-)
+logger = logging.getLogger(__name__)
+
+
+_SHORT_LINK_REGEX = re.compile(r"^/links/(?P<short_link_key>.+)$")
 
 
 def resolve_short_link(url: str) -> str:
-    match = re.match(_SHORT_LINK_REGEX, url)
-    if match is None:
-        return url
-    else:
-        webknossos_url = match.group("webknossos_url")
+    try:
+        parts = urlparse(url)
+        if parts.scheme not in ["http", "https"]:
+            return url
+        if parts.netloc == "":
+            return url
+
+        match = re.match(_SHORT_LINK_REGEX, parts.path)
+        if match is None:
+            return url
+
+        webknossos_url = f"{parts.scheme}://{parts.netloc}"
         short_link_key = match.group("short_link_key")
         with webknossos_context(url=webknossos_url):
             client = _get_generated_client()
@@ -23,3 +33,6 @@ def resolve_short_link(url: str) -> str:
             )
         assert response is not None, f"Could not resolve short link {url}."
         return response.long_link
+    except Exception as e:
+        logger.warning(f"Got an error during short link resolution of link {url}: {e}")
+        return url
