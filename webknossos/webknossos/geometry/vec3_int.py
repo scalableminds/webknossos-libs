9a33fa54807c9470ca28519609abe38479bed0ae
Norman Rzepka <code@normanrz.com>
2023-02-10 13:51:01 +0100

    Apply suggestions from code review
    Co-authored-by: Jonathan Striebel <jstriebel@users.noreply.github.com>

--- webknossos/webknossos/geometry/vec3_int.py
+++ webknossos/webknossos/geometry/vec3_int.py
@@ -135,7 +135,8 @@ class Vec3Int(tuple):
     def __sub__(self, other: Union[int, "Vec3IntLike"]) -> "Vec3Int":
         return self._element_wise(other, sub)
 
-    # note: (arguments incompatible with superclass, do not multiply Vec3Int with a plain tuple! Hence the type:ignore)
+    # Note: When multiplying regular tuples with an int those are repeated,
+    # which is a different behavior in the superclass! Hence the type:ignore.
     def __mul__(self, other: Union[int, "Vec3IntLike"]) -> "Vec3Int":  # type: ignore[override]
         return self._element_wise(other, mul)
 
