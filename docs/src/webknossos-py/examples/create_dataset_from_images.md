60e78be6f76e1ff148547fd77f90f70716c088e0
Mark Bader <markbader64@gmail.com>
2023-05-04 12:03:06 +0200

    Add examples for Dataset functions in documentation.  (#901)
    * Add example for Dataset functions in documentation.
    
    * Update docs/src/webknossos-py/examples/image_list_to_wkw.md
    
    Co-authored-by: Tom Herold <tom.herold@scalableminds.com>
    
    * Add examples to mkdocs.yml and resolve issues.
    
    * Add tests to ensure correct behaviour of examples.
    
    * Refine markdown of image stack to dataset example.
    
    * Format test_example.py with black version 23.1.0.
    
    ---------
    
    Co-authored-by: Tom Herold <tom.herold@scalableminds.com>

---
+++ docs/src/webknossos-py/examples/create_dataset_from_images.md
@@ -0,0 +1,9 @@
+# Create Dataset from Images
+
+This example shows how to [convert a folder of images into a new dataset](../../api/webknossos/dataset/dataset.md#Dataset.from_images).
+
+```python
+--8<--
+webknossos/examples/create_dataset_from_images.py
+--8<--
+```
---
+++ webknossos/examples/create_dataset_from_images.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from webknossos import Dataset
+from webknossos.dataset import COLOR_CATEGORY
+
+INPUT_DIR = Path(__file__).parent.parent / "testdata" / "tiff"
+OUTPUT_DIR = Path("testoutput/my_tiff_dataset")
+
+
+def main() -> None:
+    """Convert a folder of image files to a WEBKNOSSOS dataset."""
+    dataset = Dataset.from_images(
+        input_path=INPUT_DIR,
+        output_path=OUTPUT_DIR,
+        voxel_size=(11, 11, 11),
+        layer_category=COLOR_CATEGORY,
+        compress=True,
+    )
+
+    print(f"Saved {dataset.name} at {dataset.path}.")
+
+    # dataset.upload()
+
+
+if __name__ == "__main__":
+    main()
