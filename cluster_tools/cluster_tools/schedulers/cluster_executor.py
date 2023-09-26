98f9692567ba2ffab9e01cd3a33528385923f772
Norman Rzepka <code@normanrz.com>
2023-09-26 11:40:59 +0200

    fixes

--- cluster_tools/cluster_tools/schedulers/cluster_executor.py
+++ cluster_tools/cluster_tools/schedulers/cluster_executor.py
@@ -142,7 +142,7 @@ class ClusterExecutor(futures.Executor):
             self.meta_data["logging_setup_fn"] = kwargs["logging_setup_fn"]
 
     @classmethod
-    def as_completed(cls, futs: List[Future[_T]]) -> Iterator[Future[_T]]:
+    def as_completed(cls, futs: List["Future[_T]"]) -> Iterator["Future[_T]"]:
         return futures.as_completed(futs)
 
     @classmethod
