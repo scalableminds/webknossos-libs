056fb0359ca41040bd7e7b8617ff796f27ae2525
sm1lla <foxsmilla@gmail.com>
2023-10-24 16:02:44 +0200

    Add env variable for chunks per shard

--- webknossos/webknossos/dataset/defaults.py
+++ webknossos/webknossos/dataset/defaults.py
@@ -6,10 +6,12 @@ from .data_format import DataFormat
 DEFAULT_WKW_FILE_LEN = 32
 DEFAULT_CHUNK_SHAPE = Vec3Int.full(32)
 DEFAULT_DATA_FORMAT = (
-    DataFormat.WKW if not os.environ.get("DATA_FORMAT") == "zarr3" else DataFormat.Zarr3
+    DataFormat.WKW
+    if not os.environ.get("WK_DEFAULT_DATA_FORMAT") == "zarr3"
+    else DataFormat.Zarr3
 )
-DEFAULT_CHUNKS_PER_SHARD = (
-    Vec3Int.full(32) if DEFAULT_DATA_FORMAT == DataFormat.WKW else Vec3Int.full(16)
+DEFAULT_CHUNKS_PER_SHARD = Vec3Int.full(
+    int(os.environ.get("WK_DEFAULT_CHUNKS_PER_SHARD", 32))
 )
 DEFAULT_CHUNKS_PER_SHARD_ZARR = Vec3Int.full(1)
 DEFAULT_CHUNKS_PER_SHARD_FROM_IMAGES = Vec3Int(128, 128, 1)
