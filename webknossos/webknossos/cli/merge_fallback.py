b07da212922dddb4c2680302cc84e74a7b82d71a
Eric Jelli <eric.jelli@mpinb.mpg.de>
2024-02-08 12:08:08 +0100

    working poc with issues remaining

--- webknossos/webknossos/cli/merge_fallback.py
+++ webknossos/webknossos/cli/merge_fallback.py
@@ -9,12 +9,16 @@ from typing import Any, List, Optional, Tuple
 import typer
 from cluster_tools import Executor
 from typing_extensions import Annotated
+from rich.progress import track
+
 
 from ..annotation import Annotation
 from ..dataset import Dataset, MagView
 from ..geometry import BoundingBox, Mag
 from ..utils import get_executor_for_args
 from ._utils import DistributionStrategy, parse_path
+
+logging.basicConfig(level=logging.INFO)
 
 
 def main(
@@ -35,10 +39,10 @@ def main(
             parser=parse_path,
         ),
     ],
-    fallback_dataset: Annotated[
+    dataset_folder: Annotated[
         Any,
         typer.Argument(
-            help="Path to your WEBKNOSSOS dataset with fallback layer(s).",
+            help="Path to your WEBKNOSSOS dataset folder.",
             show_default=False,
             parser=parse_path,
         ),
@@ -105,10 +109,8 @@ def main(
         annotation.export_volume_layer_to_dataset(output_dataset)
 
     else:
-        logging.info(
-            f"Merge volume layer {volume_layer_name} with fallback layer {fallback_layer_name} from {fallback_dataset}"
-        )
-        fallback_layer = Dataset.open(fallback_dataset).get_layer(fallback_layer_name)
+        fallback_dataset_path = dataset_folder / annotation.dataset_name
+        fallback_layer = Dataset.open(fallback_dataset_path).get_layer(fallback_layer_name)
 
         with get_executor_for_args(args=executor_args) as executor:
             if volume_layer.zip is None:
@@ -118,31 +120,43 @@ def main(
                 )
 
             else:
-                with annotation.temporary_volume_layer_copy(
+                tmp_annotation_layer_name = f"{annotation.name}-TMP"
+                logging.info(f"Unpack annotation layer {volume_layer_name} temporarily in {output_dataset.name} as {tmp_annotation_layer_name}")
+                # NOTE(erjel): Cannot use "temporary_volume_layer_copy" here, since tmp folders
+                # might not be accessible from slurm compute nodes.
+                input_annotation_layer = annotation.export_volume_layer_to_dataset(
+                    output_dataset, 
+                    layer_name=tmp_annotation_layer_name,
                     volume_layer_name=volume_layer_name
-                ) as input_annotation_layer:
-                    input_annotation_mag = input_annotation_layer.get_finest_mag()
-                    fallback_mag = fallback_layer.get_mag(input_annotation_mag.mag)
+                )
 
-                    output_layer = output_dataset.add_layer_like(
-                        fallback_layer, fallback_layer.name
-                    )
-                    output_mag = output_layer.add_copy_mag(fallback_mag, compress=True)
+                input_annotation_mag = input_annotation_layer.get_finest_mag()
+                fallback_mag = fallback_layer.get_mag(input_annotation_mag.mag)
 
-                    merge_mags(output_mag, input_annotation_mag, executor)
+                logging.info(f"Create layer {fallback_layer.name} in {output_dataset.path}")
+                output_layer = output_dataset.add_layer_like(
+                    fallback_layer, fallback_layer.name
+                )
+                logging.info(f"Copy Mag {fallback_mag.mag} from {fallback_layer.path} to {output_layer.path}")
+                output_mag = output_layer.add_copy_mag(
+                    fallback_mag,
+                    compress=True,
+                    executor=executor,
+                )
 
+                merge_mags(output_mag, input_annotation_mag, executor)
+
+                ## TODO(erjel): Is there no blocking until all executor tasks are done?
+                #logging.info("Delete temporary annotation layer")
+                #output_dataset.delete_layer(tmp_annotation_layer_name)
+                #logging.info("Done.")
 
 def merge_mags(
     output_mag: MagView,
     input_annotation_mag: MagView,
     executor: Executor,
 ) -> None:
-    bboxes = list(bbox for bbox in input_annotation_mag.get_bounding_boxes_on_disk())
-
-    shards_with_bboxes = BoundingBox.group_boxes_with_aligned_mag(
-        bboxes, Mag(output_mag.info.shard_size)
-    )
-
+    
     assert all(
         input_annotation_mag.info.chunks_per_shard.to_np() == 1
     ), "volume annotation must have file_len=1"
@@ -153,22 +167,27 @@ def merge_mags(
         input_annotation_mag.mag == output_mag.mag
     ), f"Volume annotation mag {input_annotation_mag.mag} must match the fallback layer mag {output_mag.mag}"
 
-    executor.map(
-        partial(merge_chunk, mag_in=input_annotation_mag, mag_out=output_mag),
-        shards_with_bboxes.items(),
+    logging.info(f"Scan disk for annotation shards.")
+    bboxes = list(bbox for bbox in input_annotation_mag.get_bounding_boxes_on_disk())
+
+    logging.info(f"Grouping {len(bboxes)} bboxes according to output shards.")
+    shards_with_bboxes = BoundingBox.group_boxes_with_aligned_mag(
+        bboxes, Mag(output_mag.info.shard_size * output_mag.mag) 
     )
+
+    args = [(input_annotation_mag, output_mag, shard, bboxes) for shard, bboxes in shards_with_bboxes.items()]
+
+    logging.info(f"Merging {len(args)} shards.")        
+    executor.map(merge_chunk, args)
 
 
 def merge_chunk(
-    mag_in: MagView,
-    mag_out: MagView,
-    shards_with_bboxes: Tuple[BoundingBox, List[BoundingBox]],
+    args: Tuple[MagView, MagView, BoundingBox, List[BoundingBox]]
 ) -> None:
-    shard, bboxes = shards_with_bboxes
+    mag_in, mag_out, shard, bboxes = args
+    data_buffer = mag_out.read(absolute_bounding_box=shard)[0]
 
-    data_buffer = mag_in.read(absolute_bounding_box=shard)[0]
-
-    for bbox in bboxes:
+    for bbox in track(bboxes, description="Processing..."):
         read_data = mag_in.read(absolute_bounding_box=bbox)[0]
         data_buffer[
             bbox.offset(-shard.topleft).in_mag(mag_in.mag).to_slices()
