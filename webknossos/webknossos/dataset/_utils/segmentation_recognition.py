49a808fbfca637e51248c36c75ff05bf706e1fdd
markbader <markbader64@gmail.com>
2024-02-28 11:30:58 +0100

    Adapt sample size according to view size.

--- webknossos/webknossos/dataset/_utils/segmentation_recognition.py
+++ webknossos/webknossos/dataset/_utils/segmentation_recognition.py
@@ -7,7 +7,9 @@ from webknossos.dataset.view import View
 from webknossos.geometry.vec3_int import Vec3Int
 
 NUM_SAMPLES = 20
-THRESHOLD = 1 / 100 # more unique values per voxel than this value means color, less segmentation
+THRESHOLD = (
+    1 / 100
+)  # more unique values per voxel than this value means color, less segmentation
 SAMPLE_SIZE = Vec3Int(16, 16, 16)
 MAX_FAILS = 100
 
@@ -22,10 +24,10 @@ def guess_if_segmentation_from_view(view: View) -> bool:
 
 
 def sample_distinct_values_per_vx(view: View) -> float:
+    sample_size_for_view = view.size.pairmin(SAMPLE_SIZE)
     min_x, min_y, min_z = view.bounding_box.topleft
-    max_x, max_y, max_z = view.bounding_box.topleft + (
-        view.bounding_box.size - SAMPLE_SIZE
-    ).pairmin(Vec3Int(1, 1, 1))
+    max_x, max_y, max_z = view.bounding_box.bottomright - sample_size_for_view
+
     distinct_color_values = 0
     valid_sample_count = 0
     inspected_voxel_count = 0
@@ -34,13 +36,15 @@ def sample_distinct_values_per_vx(view: View) -> float:
     while valid_sample_count < NUM_SAMPLES:
         if invalid_sample_count > MAX_FAILS:
             raise RuntimeError("Failed to find enough valid samples.")
-        absolute_offset = Vec3Int(
+        offset = Vec3Int(
             random.randint(min_x, max_x),
             random.randint(min_y, max_y),
             random.randint(min_z, max_z),
         )
-        data = view.read(size=SAMPLE_SIZE, absolute_offset=absolute_offset)
+        data = view.read(size=sample_size_for_view, absolute_offset=offset)
+
         distinct_color_values_in_sample = np.unique(data)
+
         if (
             len(distinct_color_values_in_sample) == 1
             and distinct_color_values_in_sample[0] == 0
