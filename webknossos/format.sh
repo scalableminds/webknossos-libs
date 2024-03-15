dd775cf230792c8122cc14b0f982bff68ec96c85
Tom Herold <tom.herold@scalableminds.com>
2024-03-15 08:48:26 +0100

    applied PR feedback

--- webknossos/format.sh
+++ webknossos/format.sh
@@ -4,6 +4,6 @@ set -eEuo pipefail
 if [ $# -eq 1 ] && [ "$1" = "check" ]; then
     poetry run ruff format --check .
 else
-    poetry run ruff check --fix .
+    poetry run ruff check --select I --fix . # format the imports 
     poetry run ruff format .
 fi
