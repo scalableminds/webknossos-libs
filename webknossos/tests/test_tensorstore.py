74b7e5e97f58157712e7d76945d8a00f8d03d19a
brokkoli71 <brokkoli71b@gmail.com>
2024-08-23 13:35:58 +0200

    run test_tensorstore in ci

--- webknossos/tests/test_tensorstore.py
+++ webknossos/tests/test_tensorstore.py
@@ -17,20 +17,10 @@ import numpy as np
 import pytest
 
 from .constants import TESTDATA_DIR
-from .dataset.test_dataset_download_upload_remote import sample_dataset
-
-@pytest.fixture(scope="module")
-def sample_bbox() -> wk.BoundingBox:
-    return wk.BoundingBox((2807, 4352, 1794), (10, 10, 10))
-
-@pytest.fixture(scope="module")
-def sample_dataset(sample_bbox: wk.BoundingBox) -> Iterator[wk.Dataset]:
-    url = "https://webknossos.org/datasets/scalable_minds/l4_sample_dev"
-    with TemporaryDirectory() as temp_dir:
-        yield wk.Dataset.download(url, path=Path(temp_dir) / "ds", bbox=sample_bbox)
+from .dataset.test_dataset_download_upload_remote import sample_dataset, sample_bbox
 
 
-def test_remote_tensorstore(sample_dataset: wk.Dataset) -> None:
+def test_remote_tensorstore(sample_dataset: wk.Dataset):
     time_str = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
     sample_dataset.upload(
         new_dataset_name=f"test_remote_metadata_{time_str}"
