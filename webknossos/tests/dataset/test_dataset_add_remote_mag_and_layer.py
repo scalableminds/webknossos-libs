bf5c239f6480fdbefedbc4a6ce6fa195f251d368
Michael Büßemeyer <frameworklinux+MichaelBuessemeyer@users.noreply.github.com>
2024-09-25 14:40:06 +0200

    re-add flaky add remote layer/mag tests

--- webknossos/tests/dataset/test_dataset_add_remote_mag_and_layer.py
+++ webknossos/tests/dataset/test_dataset_add_remote_mag_and_layer.py
@@ -3,6 +3,7 @@ from tempfile import TemporaryDirectory
 from typing import Iterator
 
 import pytest
+from upath import UPath
 
 import webknossos as wk
 from webknossos import Dataset, MagView
@@ -66,6 +67,33 @@ def test_add_remote_mags_from_mag_view(
         ), "Added remote mag's path does not match remote path of mag added."
 
 
+@pytest.mark.skip(
+    reason="The test is flaky on the when trying to fetch the required datasource-properties.json from data-humerus.webknossos.org. Disable it for now."
+)
+def test_add_remote_mags_from_path(
+    sample_remote_mags: list[wk.MagView],
+    sample_remote_dataset: wk.Dataset,
+) -> None:
+    for remote_mag in sample_remote_mags:
+        mag_path = remote_mag.path
+        layer_type = remote_mag.layer.category
+        assert is_remote_path(mag_path), "Remote mag does not have remote path."
+        # Additional .parent calls are needed as the first .parent only removes the trailing slash.
+        layer_name = f"test_remote_layer_{mag_path.parent.name}_{mag_path.name}_path"
+        new_layer = sample_remote_dataset.add_layer(
+            layer_name,
+            layer_type,
+            data_format=remote_mag.info.data_format,
+            dtype_per_channel=remote_mag.get_dtype(),
+        )
+        new_layer.add_remote_mag(str(remote_mag.path))
+        added_mag = sample_remote_dataset.layers[layer_name].mags[remote_mag.mag]
+        # checking whether the added_mag.path matches the mag_url with or without a trailing slash.
+        assert (
+            added_mag.path == mag_path or added_mag.path == mag_path.parent
+        ), "Added remote mag's path does not match remote path of mag added."
+
+
 def test_add_remote_layer_from_object(
     sample_remote_layer: list[wk.Layer], sample_remote_dataset: wk.Dataset
 ) -> None:
@@ -77,4 +105,21 @@ def test_add_remote_layer_from_object(
         assert (
             is_remote_path(new_layer.path)
             and layer.path.as_uri() == new_layer.path.as_uri()
+        ), "Added layer should have a remote path matching the remote layer added."
+
+
+@pytest.mark.skip(
+    reason="The test is flaky on the when trying to fetch the required datasource-properties.json from data-humerus.webknossos.org. Disable it for now."
+)
+def test_add_remote_layer_from_path(
+    sample_remote_layer: list[wk.Layer],
+    sample_remote_dataset: wk.Dataset,
+) -> None:
+    for layer in sample_remote_layer:
+        assert is_remote_path(layer.path), "Remote mag does not have remote path."
+        layer_name = f"test_remote_layer_{layer.category}_path"
+        sample_remote_dataset.add_remote_layer(UPath(layer.path), layer_name)
+        new_layer = sample_remote_dataset.layers[layer_name]
+        assert (
+            is_remote_path(new_layer.path) and new_layer.path == layer.path
         ), "Added layer should have a remote path matching the remote layer added."
