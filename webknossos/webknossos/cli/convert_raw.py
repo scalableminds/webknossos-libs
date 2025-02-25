b04d1f558d2350722b6bb427a29992a2baf5e437
Norman Rzepka <code@normanrz.com>
2025-02-25 11:11:41 +0100

    change default data_format in cli

--- webknossos/webknossos/cli/convert_raw.py
+++ webknossos/webknossos/cli/convert_raw.py
@@ -15,7 +15,11 @@ from webknossos.dataset.length_unit import LengthUnit
 from webknossos.dataset.properties import DEFAULT_LENGTH_UNIT_STR, VoxelSize
 
 from ..dataset import DataFormat, Dataset, MagView, SamplingModes
-from ..dataset.defaults import DEFAULT_CHUNK_SHAPE, DEFAULT_CHUNKS_PER_SHARD
+from ..dataset.defaults import (
+    DEFAULT_CHUNK_SHAPE,
+    DEFAULT_CHUNKS_PER_SHARD,
+    DEFAULT_DATA_FORMAT,
+)
 from ..geometry import BoundingBox, Mag, Vec3Int
 from ..utils import (
     get_executor_for_args,
@@ -186,7 +190,7 @@ def main(
         typer.Option(
             help="Data format to store the target dataset in.",
         ),
-    ] = "wkw",  # type:ignore
+    ] = str(DEFAULT_DATA_FORMAT),  # type:ignore
     chunk_shape: Annotated[
         Vec3Int,
         typer.Option(
--- webknossos/webknossos/cli/convert_zarr.py
+++ webknossos/webknossos/cli/convert_zarr.py
@@ -17,7 +17,11 @@ from webknossos.dataset.length_unit import LengthUnit
 from webknossos.dataset.properties import DEFAULT_LENGTH_UNIT_STR, VoxelSize
 
 from ..dataset import DataFormat, Dataset, MagView, SegmentationLayer
-from ..dataset.defaults import DEFAULT_CHUNK_SHAPE, DEFAULT_CHUNKS_PER_SHARD
+from ..dataset.defaults import (
+    DEFAULT_CHUNK_SHAPE,
+    DEFAULT_CHUNKS_PER_SHARD,
+    DEFAULT_DATA_FORMAT,
+)
 from ..geometry import BoundingBox, Mag, Vec3Int
 from ..utils import get_executor_for_args, wait_and_ensure_success
 from ._utils import (
@@ -172,7 +176,7 @@ def main(
         typer.Option(
             help="Data format to store the target dataset in.",
         ),
-    ] = "wkw",  # type:ignore
+    ] = str(DEFAULT_DATA_FORMAT),  # type:ignore
     chunk_shape: Annotated[
         Vec3Int,
         typer.Option(
