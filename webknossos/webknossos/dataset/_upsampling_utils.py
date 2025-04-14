f01a0768badeffb013f1e442b638c84b470b6fa9
markbader <markbader64@gmail.com>
2025-04-14 22:45:52 +0200

    rewrite with chunk method.

--- webknossos/webknossos/dataset/_upsampling_utils.py
+++ webknossos/webknossos/dataset/_upsampling_utils.py
@@ -32,56 +32,38 @@ def upsample_cube_job(
     assert all(1 >= f for f in mag_factors), (
         f"mag_factors ({mag_factors}) for upsampling must be smaller than 1"
     )
+    assert buffer_shape % target_view.info.shard_shape == 0, (
+        f"buffer_shape ({buffer_shape=}) must be a multiple of ({target_view.info.shard_shape=})"
+    )
     inverse_factors = [int(1 / f) for f in mag_factors]
 
     try:
         num_channels = target_view.info.num_channels
-        target_bbox = target_view.bounding_box.align_with_mag(
-            target_view.info.shard_shape, True
-        )
-        shape_xyz = target_bbox.size // target_view.mag
 
-        tiles = product(
-            *list(
-                [
-                    list(range(0, math.ceil(length)))
-                    for length in shape_xyz.to_np() / buffer_shape.to_np()
-                ]
-            )
-        )
-
-        for tile in tiles:
-            tile_bbox = target_view.bounding_box.offset(
-                Vec3Int(tile) * buffer_shape * target_view.mag
-            ).with_size_xyz(buffer_shape * target_view.mag)
-
-            bbox = target_bbox.intersected_with(tile_bbox)
-            shape = (num_channels,) + (
-                bbox.in_mag(target_view.mag)
-            ).size.to_tuple()  # shape of file buffer in target mag
-            file_buffer = np.zeros(
-                shape, dtype=target_view.get_dtype()
-            )  # zero filled file buffer
+        for chunk in target_view.bounding_box.chunk(
+            buffer_shape * target_view.mag, buffer_shape * target_view.mag
+        ):
+            shape = (num_channels,) + (chunk.in_mag(target_view.mag)).size.to_tuple()
+            file_buffer = np.zeros(shape, dtype=target_view.get_dtype())
             cube_buffer_channels = source_view.read_xyz(
-                absolute_bounding_box=bbox,
-            )  # read from source view to receive data in shape of file buffer * mag_factors
+                absolute_bounding_box=chunk,
+            )
 
             for channel_index in range(num_channels):
                 cube_buffer = cube_buffer_channels[channel_index]
 
                 if not np.all(cube_buffer == 0):
-                    # Upsample the buffer
                     data_cube = upsample_cube(cube_buffer, inverse_factors)
 
-                    buffer_bbox = target_view.bounding_box.with_topleft_xyz(
-                        source_offset * inverse_factors
-                    ).with_size_xyz(data_cube.shape)
+                    buffer_bbox = chunk.with_topleft(Vec3Int.zeros()).with_size_xyz(
+                        data_cube.shape
+                    )
                     data_cube = buffer_bbox.xyz_array_to_bbox_shape(data_cube)
                     file_buffer[(channel_index,) + buffer_bbox.to_slices_xyz()] = (
                         data_cube
                     )
 
-            target_view.write(file_buffer, absolute_bounding_box=bbox)
+            target_view.write(file_buffer, absolute_bounding_box=chunk)
 
     except Exception as exc:
         logging.error(
