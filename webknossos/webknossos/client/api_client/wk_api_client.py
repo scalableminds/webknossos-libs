be2a79087a2136e8e7d98a65d217182d95e0695e
valentin-pinkau <valentin.pinkau@scalableminds.com>
2025-09-16 10:09:13 +0200

    do not pass includePaths

--- webknossos/webknossos/client/api_client/wk_api_client.py
+++ webknossos/webknossos/client/api_client/wk_api_client.py
@@ -79,7 +79,7 @@ class WkApiClient(AbstractApiClient):
         return self._get_json(
             route,
             ApiDataset,
-            query={"sharingToken": sharing_token, "includePaths": True},
+            query={"sharingToken": sharing_token},
         )
 
     def dataset_id_from_name(self, *, directory_name: str, organization_id: str) -> str:
